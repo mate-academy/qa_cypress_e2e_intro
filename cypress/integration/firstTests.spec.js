ba14a51d00ccfbcf101f4cc5430beac298dc3c3d
Olha Blokhina <olha18blokhina10@gmail.com>
2022-02-17 16:44:45 +0200

    Solution Olha Blokhina

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -1,7 +1,42 @@
 /// <reference types="cypress" />
 
-describe('', () => {
-  it('', () => {
-    
+beforeEach(() => {
+  cy.visit('https://react-redux.realworld.io')
+})
+
+describe('First Test', () => {
+  it('sign in with valid data', () => {
+  cy.get (':nth-child(2) > .nav-link')
+    .click()
+    .get (':nth-child(1) > .form-control')
+    .type ('newuser2022@gmail.com')
+    .get(':nth-child(2) > .form-control')
+    .type('NewUser2022')
+    .get('.btn')
+    .click()
+
+  cy.get (':nth-child(4) > .nav-link')
+  .should('contain', 'newuser2022')
+  });
+});
+
+
+const { generateUser } = require('../support/generate')
+describe('Second test', () => {
+   it('Sign Up with faker', () => { 
+const {email, password, username} = generateUser();
+cy.get (':nth-child(3) > .nav-link')
+  .click()
+  .get(':nth-child(1) > .form-control')
+  .type (username)
+  .get(':nth-child(2) > .form-control')
+  .type (email)
+  .get(':nth-child(3) > .form-control')
+  .type (password)
+  .get ('.btn')
+  .click ()
+
+cy.get (':nth-child(4) > .nav-link')
+  .should('contain', username)
   });
 });
