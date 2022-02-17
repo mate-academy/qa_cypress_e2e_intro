4fe98ae786b619a087f729c1fc0d392fc80f6862
Andriy Polnyi <andrii.polnyy@gmail.com>
2022-02-17 20:15:22 +0200

    Polnyi Andriy

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -1,7 +1,38 @@
 /// <reference types="cypress" />
 
-describe('', () => {
-  it('', () => {
-    
+const { generateUser } = require('../support/generate');
+
+describe('Advanced level', () => {
+  it('Sign up with faker', () => {
+    const user = generateUser();
+    cy.visit('https://react-redux.realworld.io');
+    cy.get(':nth-child(3) > .nav-link')
+     .click();
+     cy.get(':nth-child(1) > .form-control')
+     .type(user.username);
+     cy.get(':nth-child(2) > .form-control')
+     .type(user.email);
+     cy.get(':nth-child(3) > .form-control')
+     .type(user.password);
+     cy.get('.btn')
+     .click();
+     cy.get(':nth-child(4) > .nav-link')
+     .should('contain', user.username);
+  });
+});
+
+describe('Basic level', () => {
+  it('Sign in with valid date', () => {
+    cy.visit('https://react-redux.realworld.io');
+    cy.get(':nth-child(2) > .nav-link')
+     .click();
+    cy.get(':nth-child(1) > .form-control')
+     .type('qentin@gmail.com');
+    cy.get(':nth-child(2) > .form-control')
+     .type('151636Ann');
+    cy.get('.btn')
+     .click();
+    cy.get(':nth-child(4) > .nav-link')
+    .should('contain', 'qentin');
   });
 });
