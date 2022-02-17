d2e50462b9392b0356f9e65eb34e6b69f4d6410f
OksenchukPavel <oksen4ukpavel@gmail.com>
2022-02-17 21:13:26 +0200

    Pasha Oksenchuk Cypress first test

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -5,3 +5,41 @@ describe('', () => {
     
   });
 });
+/// <reference types="cypress" />
+
+const { generateUser } = require('../support/generate');
+
+describe('Advanced level', () => {
+  it('Sign up with faker', () => {
+    const user = generateUser();
+    cy.visit('https://react-redux.realworld.io');
+    cy.get(':nth-child(3) > .nav-link')
+     .click();
+     cy.get(':nth-child(1) > .form-control')
+     .type(user.username);
+     cy.get(':nth-child(2) > .form-control')
+     .type(user.email);
+     cy.get(':nth-child(3) > .form-control')
+     .type(user.password);
+     cy.get('.btn')
+     .click();
+     cy.get(':nth-child(4) > .nav-link')
+     .should('contain', user.username);
+  });
+});
+
+describe('Basic level', () => {
+  it('Sign in with valid date', () => {
+    cy.visit('https://react-redux.realworld.io');
+    cy.get(':nth-child(2) > .nav-link')
+     .click();
+    cy.get(':nth-child(1) > .form-control')
+     .type('oksen423ukpavel@gmail.com');
+    cy.get(':nth-child(2) > .form-control')
+     .type('Mate123!');
+    cy.get('.btn')
+     .click();
+    cy.get(':nth-child(4) > .nav-link')
+    .should('contain', 'HomeWork');
+  });
+});
