2806a607787b3b1dd6d94e455bff74afcd6f1be1
McBorov <nazar.zaremba27032002@gmail.com>
2022-02-18 22:41:41 +0200

    Solution

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -1,7 +1,49 @@
 /// <reference types="cypress" />
+const faker = require('faker');
+const { generateUser } = require('../support/generate');
 
-describe('', () => {
-  it('', () => {
-    
+describe('Registration and autorization', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/register')
+  });
+
+  it.only('Registration and autorization test', () => {
+    const { email, password, username} = generateUser()
+
+    cy.get('[placeholder="Username"]')
+      .type(username);
+
+    cy.get('[placeholder="Email"]')
+      .type(email);
+
+    cy.get('[placeholder="Password"]')
+      .type(password);
+
+    cy.contains('.btn', 'Sign in')
+      .click();
+
+    cy.get(`[href="#@${username}"]`)
+      .click();
+
+    cy.get('.btn')
+      .click();
+
+    cy.get('.btn-outline-danger')
+      .click();
+
+    cy.get('[href="#login"]')
+      .click();
+
+    cy.get('[placeholder="Email"]')
+      .type(email);
+
+    cy.get('[placeholder="Password"]')
+      .type(password);
+
+    cy.get('.btn')
+      .click();
+
+    cy.get(`[href="#@${username}"]`)
+      .should('contain.text', `${username}`);
   });
 });
