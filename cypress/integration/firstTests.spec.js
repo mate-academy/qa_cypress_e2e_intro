d91bd41ab44dcf0eb46d7dbce992745f191b1436
Larysa Khomenko <larisa.iovchu@gmail.com>
2022-05-25 18:20:11 +0300

    Solution

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -1,7 +1,27 @@
 /// <reference types="cypress" />
 
-describe('', () => {
-  it('', () => {
+describe('Sign in form', () => {
+  it('should allow to login', () => {
+    cy.visit('https://react-redux.realworld.io/#/')
+
+    cy.get(':nth-child(2) > .nav-link')
+      .click()
+
+    cy.url()
+      .should('include', 'login?')
+
+    cy.get(':nth-child(1) > .form-control') 
+      .type('jucehesaw@mail.ua')
+      .should('have.value', 'jucehesaw@mail.ua')
     
+    cy.get(':nth-child(2) > .form-control')
+      .type('jucehesaw')
+      .should('have.value', 'jucehesaw')
+
+    cy.get('.btn')
+      .click()
+
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', 'jucehesaw')
   });
 });
