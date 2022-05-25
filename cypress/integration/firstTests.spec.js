0de5a076e53527b533324be4c8a7432bc971bfba
Yulia Bylym <bylym.yulia@gmail.com>
2022-05-25 19:46:16 +0300

    Solution Anvanced

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -1,7 +1,37 @@
 /// <reference types="cypress" />
 
-describe('', () => {
-  it('', () => {
-    
+describe('Sign in', () => {
+  beforeEach(() =>
+    cy.visit('/#/login')
+  )
+  const username = 'test_yumi'
+  const email = `${username}@mail.com`
+  const password = 'Pa$$w0rd!1'
+
+  it('should open Sign In page', () => {
+    cy.url()
+    .should('include', '/login')
+          
+    cy.get('h1')
+    .should('contain.text', 'Sign In')
+  });
+  
+  it('should allow log into the account', () => {
+    cy.get(`input[placeholder='Email']`)
+      .click()
+      .type(email)
+      .should('have.value', email)
+
+    cy.get(`input[placeholder='Password']`)
+      .click()
+      .type(password)
+      .should('have.value', password)
+
+    cy.get(`button[type='submit']`)
+      .should('contain.text', 'Sign in')
+      .click()
+
+    cy.get(`a[href='#@${username}']`)
+      .should('contain.text', username)     
   });
 });
