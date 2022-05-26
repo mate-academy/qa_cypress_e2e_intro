448c582ed2abc2e343706b6489e7d271cb0328a1
Orest Mykhailyshyn <orest.mp@gmail.com>
2022-05-26 16:38:37 +0300

    Solution

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -1,7 +1,28 @@
 /// <reference types="cypress" />
 
-describe('', () => {
-  it('', () => {
-    
+describe('Sign in form', () => {
+  it('should allow to log in', () => {
+    cy.visit('/');
+
+    cy.get(':nth-child(2) > .nav-link')
+      .click();
+
+    cy.url()
+      .should('include', '/login');
+
+    cy.get(':nth-child(1) > .form-control')
+      .type('UserTesterovych@mail.com')
+      .should('have.value', 'UserTesterovych@mail.com')
+
+    cy.get(':nth-child(2) > .form-control')
+      .type('Pa$$word1')
+      .should('have.value', 'Pa$$word1')
+      
+    cy.get('.btn')
+      .contains('Sign in')
+      .click();
+
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', 'UserTesterovych');
   });
 });
