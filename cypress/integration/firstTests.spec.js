3c8aaa1b17e5bbe728f19eba153b9f0ad6f09773
Oleg Lukyanchuk <oleeluk@gmail.com>
2022-05-29 19:24:24 +0300

    Solution

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -1,7 +1,29 @@
 /// <reference types="cypress" />
 
-describe('', () => {
-  it('', () => {
+describe('first test', () => {
+  beforeEach(() =>{
+    cy.visit('/')
+  })
+  it('test', () => {
+    cy.get('h1')
+      .should('contain.text', 'conduit');
+
+    cy.contains('Sign in')
+      .click();
     
+    cy.get(':nth-child(1) > .form-control')
+      .type('qwe_rty@gmail.com')
+      .should('have.value', 'qwe_rty@gmail.com')
+
+    cy.get(':nth-child(2) > .form-control')
+      .type('123456')
+      .should('have.value', '123456')
+
+    cy.contains('.btn', 'Sign in')
+      .click()
+
+    cy.contains('qwe_rty')
+      .should('contain.text', 'qwe_rty');
   });
+  
 });
