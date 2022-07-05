3806cbb20f239e24c67e4f2521f0a8bd2b7cba87
Yevgeniyaa Yevdokimova <jane.evdokimova277@gmail.com>
2022-07-05 12:57:17 +0300

    Fixes after code review

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  
+    e2e: {
+      baseUrl: 'https://react-redux.realworld.io/',
+      viewportWidth: 1024,
+      viewportHeight: 768
+    }
+  
+
+});
+
