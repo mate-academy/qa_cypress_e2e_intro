2bd42ab348657847d5766c48866f16b5c21840a1
Yana Shapovalova <shapovalovayanaa@gmail.com>
2022-07-05 12:53:18 +0300

    cypress_task_new

--- cypress/e2e/firstTests.cy.js
+++ cypress/e2e/firstTests.cy.js
@@ -3,17 +3,20 @@ describe('My First Test', () => {
   it('Visit "Sign in" page', () => {
     cy.visit('https://react-redux.realworld.io')
     cy.get(':nth-child(2) > .nav-link')
-    .click()
+      .click()
 
     cy.get(':nth-child(1) > .form-control')
-    .type('yana@gmail.com')
-    .should('have.value', 'yana@gmail.com')
+      .type('yana@gmail.com')
+      .should('have.value', 'yana@gmail.com')
 
     cy.get(':nth-child(2) > .form-control')
-    .type('yana')
-    .should('have.value', 'yana')
+      .type('yana')
+      .should('have.value', 'yana')
 
     cy.get('.btn')
-    .click()
+      .click()
+
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', 'yana_test')
   });
 });
