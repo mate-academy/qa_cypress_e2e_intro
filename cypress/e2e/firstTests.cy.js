cc86a0b757b7188ef741712a9bac5cf283c33f02
Alex Kudryavtsev <33124227+iKakoc@users.noreply.github.com>
2022-07-05 13:08:15 +0300

    Update firstTests.cy.js
    new

--- cypress/e2e/firstTests.cy.js
+++ cypress/e2e/firstTests.cy.js
@@ -9,7 +9,7 @@ describe('', () => {
   it('', () => {
    cy.visit('https://react-redux.realworld.io/#/login');
    cy.get(':nth-child(1) > .form-control')
-     .type('kakoc.pwn@gmail.com.com')
+     .type('kakoc.pwn@gmail.com')
     cy.get(':nth-child(2) > .form-control')
     .type('321678')
     cy.get('.btn')
