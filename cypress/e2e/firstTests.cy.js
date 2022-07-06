37fc526b6a60a4de7624e80ed6ffe3de2f164561
Marina Bidenko <bidenko.bmw@gmail.com>
2022-07-06 19:27:30 +0300

    Solution

---
+++ cypress/e2e/firstTests.cy.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+
+const { generateUser } = require('../support/generate');
+
+describe('Sign in', () => {
+  beforeEach(() => {
+    cy.visit('/#/login')
+  });
+  
+  it('Should allow to sign in', () => {
+    cy.get(':nth-child(1) > .form-control')
+      .type('tester_user1@gmail.com');
+    
+    cy.get(':nth-child(2) > .form-control')
+      .type('Password1!');
+
+    cy.contains('.btn', 'Sign in')
+      .click();
+
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain.text', 'tester_user1');
+  });
+});
+
+describe('Sign up', () => {
+  beforeEach(() => {
+    cy.visit('/#/register')
+  });
+  
+  it('Should allow to sign up', () => {
+    
+    const user = generateUser();
+
+    cy.get(':nth-child(1) > .form-control')
+      .type(`${user.username}`);
+    
+    cy.get(':nth-child(2) > .form-control')
+      .type(`${user.email}`);
+    
+    cy.get(':nth-child(3) > .form-control')
+      .type(`${user.password}`);
+    
+    cy.contains('.btn', 'Sign in')
+      .click();
+
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain.text', `${user.username}`);
+  });
+});
