cc30bcc35dc37bee8d6749548e60523cd7fc689b
Juliko <4juliko@gmail.com>
2022-08-25 20:03:49 +0200

    Advanced

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,17 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
   },
-})
+});
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://react-redux.realworld.io",
+  },
+});
