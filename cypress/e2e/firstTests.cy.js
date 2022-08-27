71e524ca5bb6a63286d138d85428524a073ed3af
MariiaShurabura <shurabura95@gmail.com>
2022-08-27 15:44:53 +0300

    updated

--- cypress/e2e/firstTests.cy.js
+++ cypress/e2e/firstTests.cy.js
@@ -3,27 +3,21 @@
 const username = 'tester_use';
 
 describe('user can log in with valid data', () => {
-    it('should open sign in page', () => {
+    it('should allow to login with valid data', () => {
         cy.visit('https://react-redux.realworld.io');
 
         cy.get(':nth-child(2) > .nav-link')
         .click();
-        });
 
-    it ('should fill login and password fields as valid data', () => {
         cy.get(':nth-child(1) > .form-control')
         .type('testallf97@gmail.com');
 
         cy.get(':nth-child(2) > .form-control')
         .type('Bodape42');
-        })
 
-    it ('should click on the sign in button', () => {
-    cy.get('.btn')
+        cy.get('.btn')
         .click();
-        });
 
-    it ('username should be displayed in header', () => {
         cy.get(':nth-child(4) > .nav-link')
         .should('contain', username);
     })
