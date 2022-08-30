45b2622b08f199bedc055e9af5ac8a2874025240
shevasg <ssg.shevchenko@gmail.com>
2022-08-30 17:07:00 +0300

    first home work cypress

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -1,7 +1,28 @@
 /// <reference types="cypress" />
+//const { generateUser } = require("../support/generateUser"); 
 
-describe('', () => {
-  it('', () => {
-    
+describe('Registration page', () => {
+  it('should register new user with valid data', () => {
+    //const {username, email, password } = generateUser ();
+
+    cy.visit('https://react-redux.realworld.io');
+
+      cy.get(':nth-child(2) > .nav-link')
+        .click( )
+        
+        cy.get(':nth-child(1) > .form-control')
+          .type('qazwsx2@i.ua');
+        
+        cy.get(':nth-child(2) > .form-control')
+          .type('123456');
+
+        cy.get('.btn')
+          .click();
+
+        cy.get(':nth-child(4) > .nav-link')
+          .should('contain', 'qazwsx2');     
+
+        cy.get('.navbar-brand')
+        .should('contain', 'conduit');
   });
 });
