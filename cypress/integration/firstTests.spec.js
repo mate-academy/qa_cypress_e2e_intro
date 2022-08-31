482c966f1b99c436901b93add33636bf6cc94c0c
shevasg <ssg.shevchenko@gmail.com>
2022-08-31 16:22:09 +0300

    Second commit with Advanced HW

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -1,11 +1,14 @@
 /// <reference types="cypress" />
-//const { generateUser } = require("../support/generateUser"); 
 
-describe('Registration page', () => {
-  it('should register new user with valid data', () => {
-    //const {username, email, password } = generateUser ();
+const { generateUser } = require('../support/generate');
 
-    cy.visit('https://react-redux.realworld.io');
+describe('Login page', () => {
+
+  it('should Login user with valid data', () => {   
+
+    // const {username, email, password } = generateUser ();
+
+    cy.visit('https://react-redux.realworld.io');   
 
       cy.get(':nth-child(2) > .nav-link')
         .click( )
@@ -24,5 +27,67 @@ describe('Registration page', () => {
 
         cy.get('.navbar-brand')
         .should('contain', 'conduit');
-  });
+
+        cy.get(':nth-child(3) > .nav-link')
+          .click();
+
+        cy.get('.btn-outline-danger')
+        .click();
+
+        
+
+      // --------------------------------------
+      // describe('Your Second Test', () => {
+      //   it('Sign up with faker', () => {
+      //     const user = generateUser();
+          
+      //     <your_code>
+      //   });
+      // });
+      // ```
+      // --------------------------------------
+
+
+  // it.only ('Sign up with faker', () => {
+  //         const user = generateUser ();
+          
+  //     cy.visit('https://react-redux.realworld.io');
+    
+  //       cy.get(':nth-child(3) > .nav-link')
+  //         .click();
+ 
+  //       cy.get(':nth-child(1) > .form-control')
+  //         .type(user.username);
+  
+  //       cy.get(':nth-child(2) > .form-control')
+  //         .type(user.email);
+  
+  // //       cy.get(':nth-child(3) > .form-control')
+  // //         .type(user.password);
+      
+  // //       cy.get('.btn')
+  // //         .click();
+      
+  // //       cy.get(':nth-child(4) > .nav-link')
+  // //         .should('contain', user.username)
+
+  //       });  
+
+  });   
+
+    
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
 });
---
+++ cypress/integration/secondTests.spec.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+
+// const { describe } = require("mocha");
+
+const { generateUser } = require('../support/generate');
+
+// '../support/generate'
+
+describe('Your Second Test', () => {
+
+  it('Sign up with faker', () => {
+    
+    const user = generateUser();
+             
+      cy.visit('https://react-redux.realworld.io');
+    
+        cy.get(':nth-child(3) > .nav-link')
+          .click();
+ 
+        cy.get(':nth-child(1) > .form-control')
+          .type(user.username);
+  
+        cy.get(':nth-child(2) > .form-control')
+          .type(user.email);
+  
+        cy.get(':nth-child(3) > .form-control')
+          .type(user.password);
+      
+        cy.get('.btn')
+          .click();
+      
+        cy.get(':nth-child(4) > .nav-link')
+          .should('contain', user.username)
+
+        });  
+
+  });   
