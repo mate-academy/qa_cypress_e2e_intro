6d680fae1daa1a835fe9f914ebecf1c8adf17dfc
Oleksandra Polishchuk <alex.wazaa@gmail.com>
2022-10-03 22:52:08 +0300

    Homework_cypress

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -1,7 +1,24 @@
 /// <reference types="cypress" />
 
-describe('', () => {
-  it('', () => {
-    
+describe('My first tests', () => {
+  it('should', () => {
+    cy.visit('https://react-redux.realworld.io')
   });
+
+  it('to Sign In with valid data', () => {
+    cy.get(':nth-child(2) > .nav-link')
+    .click();
+
+    cy.get(':nth-child(1) > .form-control')
+    .type('test@qa.team');
+
+    cy.get(':nth-child(2) > .form-control')
+    .type('1234Qwer!');
+
+    cy.get('.btn')
+    .click();
+
+    cy.get(':nth-child(4) > .nav-link')
+    .should('contain', '');
+  })
 });
