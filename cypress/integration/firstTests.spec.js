d9cc60d71e4a1f3c5ee550f47055084500b0b052
Nataliia Solotva <nsolotva@gmail.com>
2022-10-04 01:34:41 +0300

    'Firsxt commit'

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -1,7 +1,22 @@
 /// <reference types="cypress" />
 
-describe('', () => {
-  it('', () => {
-    
+describe ('my first test', () => {
+  before (() => {
+    cy.visit('https://react-redux.realworld.io/')
+  });
+ 
+  it('should', () => {
+    cy.contains('Sign in')
+    .click()
+    cy.get(':nth-child(1) > .form-control')
+    .type('nataliia@qa.team');
+    cy.get(':nth-child(2) > .form-control')
+    .type('12345Qwert!');
+    cy.get('.btn')
+    .click();
+    cy.get(':nth-child(4) > .nav-link')
+    .should('contain', 'nataliia123');
   });
 });
+
+
---
+++ cypress/integration/secondTests.spec.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+const { generateUser } = require('../support/generate');
+
+describe ('my second test', () => {
+
+  before (() => {
+    cy.visit('https://react-redux.realworld.io/')
+    cy.contains('Sign up')
+    .click()
+  });
+ 
+  it('should', () => {
+    const user = generateUser();
+
+    cy.get(':nth-child(1) > .form-control')
+    .type(user.username);
+    cy.get(':nth-child(2) > .form-control')
+    .type(user.email);
+    cy.get(':nth-child(3) > .form-control')
+    .type(user.password);
+    cy.get('.btn')
+    .click();
+    cy.get(':nth-child(4) > .nav-link')
+    .should('contain', user.username);
+  });
+});
+
+
