08510854dc64d5fa207cbd23abe400cafc1a1345
YehorVoroniuk <voronyuk1998@gmail.com>
2022-10-03 14:45:13 +0300

    Solution

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -1,7 +1,14 @@
 /// <reference types="cypress" />
 
-describe('', () => {
-  it('', () => {
+describe('Sign In test', () => {
+  it('should ', () => {
+    cy.visit('https://react-redux.realworld.io')
+      .get('h1')
+      .should('contain.text', 'conduit');
+
+    cy.contains('Sign in')
+      .click();
+
     
   });
 });
