204ec032b0b4e1966b6972c22f92105140ea6520
Алексей Сироткин <light@LightLand.localdomain>
2022-10-03 17:57:06 +0300

    Done

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -1,7 +1,21 @@
 /// <reference types="cypress" />
 
-describe('', () => {
-  it('', () => {
-    
-  });
-});
+describe('User should be able', () => {
+  before(() => {
+    cy.visit('https://react-redux.realworld.io')  
+})
+
+  it('to Sign In with valid data', () => {
+    cy.get(':nth-child(2) > .nav-link')
+      .click();
+
+    cy.get(':nth-child(1) > .form-control')
+      .type('TestUserEmail@qa.team');
+    cy.get(':nth-child(2) > .form-control')
+      .type('123456Qwerty!');
+    cy.get('.btn')
+      .click();
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', '');
+  })
+})
