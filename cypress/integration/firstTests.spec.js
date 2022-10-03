302b5d4e8689142313e92d08691242170046f63e
YehorVoroniuk <voronyuk1998@gmail.com>
2022-10-03 15:02:15 +0300

    Solution

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -1,14 +1,6 @@
 /// <reference types="cypress" />
 
-describe('Sign In test', () => {
-  it('should ', () => {
-    cy.visit('https://react-redux.realworld.io')
-      .get('h1')
-      .should('contain.text', 'conduit');
-
-    cy.contains('Sign in')
-      .click();
-
-    
+describe('', () => {
+  it('', () => {      
   });
 });
