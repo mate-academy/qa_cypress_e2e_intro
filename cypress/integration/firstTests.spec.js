c3616ff8e23649a9ab4e5cd98cb3ca3d87537362
Oleksandra Polishchuk <alex.wazaa@gmail.com>
2022-10-04 12:35:09 +0300

    Sign in, Firsts test

--- cypress/integration/firstTests.spec.js
+++ cypress/integration/firstTests.spec.js
@@ -1,24 +1,24 @@
 /// <reference types="cypress" />
 
-describe('My first tests', () => {
-  it('should', () => {
-    cy.visit('https://react-redux.realworld.io')
+describe('User shoul be able', () => {
+  before(() => {
+    cy.visit('https://react-redux.realworld.io');
   });
 
-  it('to Sign In with valid data', () => {
+  it('to Sign in with valid credentials', () => {
     cy.get(':nth-child(2) > .nav-link')
-    .click();
+      .click();
 
     cy.get(':nth-child(1) > .form-control')
-    .type('test@qa.team');
+      .type('alam@qa.team');
 
     cy.get(':nth-child(2) > .form-control')
-    .type('1234Qwer!');
+      .type('Qwert!123');
 
     cy.get('.btn')
-    .click();
+      .click();
 
     cy.get(':nth-child(4) > .nav-link')
-    .should('contain', '');
-  })
+      .should('contain', 'alam');
+  });
 });
