6dd945c3795a2e9e4de6810797838b0d04bb2efe
Anna Kononuchenko <horsepower619@gmail.com>
2022-10-04 13:16:42 +0300

    Solution

--- cypress/e2e/firstTests.cy.js
+++ cypress/e2e/firstTests.cy.js
@@ -1,25 +1,25 @@
 /// <reference types="cypress" />
 
 describe('My first tests', () => {
-  it('should', () => {
+  it('should open Main Page', () => {
     cy.visit('https://react-redux.realworld.io')
   });
 
-  it('to Sign In with valid data', () => {
+  it('Sign In with valid data', () => {
     cy.get(':nth-child(2) > .nav-link')
     .click();
 
     cy.get(':nth-child(1) > .form-control')
-    .type('test@qa.team');
+    .type('anna@qa.team');
 
     cy.get(':nth-child(2) > .form-control')
-    .type('1234Qwer!');
+    .type('anna1234');
 
     cy.get('.btn')
     .click();
 
     cy.get(':nth-child(4) > .nav-link')
-    .should('contain', '');
+    .should('contain', 'anna1234');
   })
 });
 
