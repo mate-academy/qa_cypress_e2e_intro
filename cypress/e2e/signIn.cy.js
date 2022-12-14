0fdf6991a41c64bbea8bcf3bf6fc46a56a9ef674
Karolina Vlasova <carolina150897@gmail.com>
2022-12-14 11:51:55 +0100

    signIn

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -3,5 +3,20 @@
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
     
+    cy.visit('https://react-redux.realworld.io/');
+    
+    cy.get(':nth-child(2) > .nav-link').click();
+    
+    cy.url().should('include', '/login');
+
+    cy.get('h1.text-xs-center');
+
+    cy.get(':nth-child(1) > .form-control').type('magic@qa.team');
+
+    cy.get(':nth-child(2) > .form-control').type('Patronus1!');
+
+    cy.get('.btn').click();
+
+    cy.get(':nth-child(4) > .nav-link');
   });
 });
