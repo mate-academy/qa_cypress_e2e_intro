34e21454ec67f4c6a0d1e82af8b5820a73d6887b
NikitaMinchenko <nikitaminchenko30@gmail.com>
2022-12-14 13:32:00 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,39 @@
 /// <reference types="cypress" />
 
+// const { should } = require("chai");
+
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  
+  beforeEach (() => {
+    cy.visit("/")
+  });
+  
+  it('should provide an ability to log in with valid credentials', () => {
+    
+    
+    cy.get("h1")
+      .should("contain.text", "conduit")
+
+    cy.contains("Sign in")
+      .click()
+
+    cy.url()
+      .should("include", "/login")
+
+    cy.get("h1")
+      .should("contain", "Sign In")
+
+    cy.get('[placeholder="Email"]')
+      .type("NikitaHello@qa.com")
+
+    cy.get('[placeholder="Password"]')
+      .type("Password1234567890!")
+
+    cy.contains('.btn', 'Sign in')
+      .click()
+
+    cy.get(':nth-child(4) > .nav-link')
+      .should("contain.text", "NikitaHello")
     
   });
 });
