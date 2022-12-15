5902f9344679016056edaa7da50783284f245085
YUrii Kachur <yurii.kachurr@gmail.com>
2022-12-15 13:40:04 +0200

    My first test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,23 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
-    
-  });
+describe('Sign in', () => {
+  
+  it('Should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io')
+
+    cy.contains('Sign in')
+    .click();
+
+    cy.get(':nth-child(1) > .form-control')
+    .type('user1@mail.com');
+
+    cy.get(':nth-child(2) > .form-control')
+    .type('password1');
+
+    cy.contains('.btn', 'Sign in')
+    .click();
+
+    cy.get(':nth-child(4) > .nav-link')
+    .should('contain', 'demoname')
+  })
 });
