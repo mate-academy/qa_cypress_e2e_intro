a8d0a1d6d78cfeb3c7dfa19ea786e225a6b9e638
Iryna Sulym <irinasulim4@gmail.com>
2022-12-18 18:48:22 +0100

    ‘Solution’

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,32 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+
+    //Visit "Sign in" page.
+    cy.visit('https://react-redux.realworld.io')
+      .get(':nth-child(2) > .nav-link')
+
+    cy.contains('Sign in')
+      .click()
+
+    cy.url()
+      .should('include', '/login')
+
+    //Fill in email and password fields as valid data.
+    cy.get(':nth-child(1) > .form-control')
+      .type('testUser4@gmail.com')
+
+    cy.get(':nth-child(2) > .form-control')
+      .type('Aa123456')
+
+    //Click on [Sign In] button
+    cy.contains('.btn', 'Sign in')
+      .click() 
+
+    //Assert your username appeared in "Site navigation (Header)" menu
+    cy.get(':nth-child(4) > .nav-link')
+    .contains('testUser4')
     
   });
 });
+
