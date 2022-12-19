6a462e4481e362432f17845eb46cc1ddd7c6c83b
Viacheslav Chernobrovin <chernobrovi87@gmail.com>
2022-12-19 23:51:44 +0200

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'k4mpar',
   e2e: {
     setupNodeEvents(on, config) {
     },
+    baseUrl: "https://react-redux.realworld.io" 
   },
 })
--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,29 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
-    
+  beforeEach(() => {
+    cy.visit('/')
   });
+  it('should provide an ability to log in', () => {
+    cy.contains('Sign in')
+      .click();
+
+    cy.url()
+      .should('include', '#/login');
+
+    cy.get('h1')
+    .should('contain.text', 'Sign In');
+
+    cy.get(':nth-child(1) > .form-control')
+      .type('sobaka-tester@sobaka.test');
+
+    cy.get(':nth-child(2) > .form-control')
+      .type('Qwe123@');
+
+    cy.contains('.btn', 'Sign in')
+      .click();
+
+      cy.get(':nth-child(4) > .nav-link')
+        .should('contain', 'rgrgrrg');
+    });
 });
