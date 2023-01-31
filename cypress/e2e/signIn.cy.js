34bd725da158d592f4ff9ad3b0368c59e7523ed4
Vlad Didkovskiy <vladgreen.vg@gmail.com>
2023-01-31 18:23:50 +0200

    login test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,32 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=6eu1fy')
+  })
+
+  it('should contain all parts', () => {
+    cy.get('h1')
+      .should('contain.text', 'Sign In')
+
+    cy.contains('a', 'Need an account?')
+      .should('exist')
+
+    cy.contains('.btn', 'Sign in')
+      .should('exist')
+  });
+
   it('should provide an ability to log in', () => {
-    
+    cy.get('[placeholder="Email"]') // Email field
+      .type('test08@gmail.com')
+
+    cy.get('[placeholder="Password"]') // Password field
+      .type('12345')
+
+    cy.contains('.btn', 'Sign in')
+      .click();
+
+    cy.contains('a', 'test08')
+      .should('exist')
   });
 });
