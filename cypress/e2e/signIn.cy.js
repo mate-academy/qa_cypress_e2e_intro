9e55990988b2cb6af5779ec573eb2ff557e5c46c
Triukhan <tryukhan.d@gmail.com>
2023-02-01 21:39:07 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,18 @@
 /// <reference types="cypress" />
 
+
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('/');
+
+    cy.contains('a', 'Sign in').should('exist').click();
     
-  });
+    cy.url().should('include', '/login');
+
+    cy.get('[placeholder  = "Email"]').type('danilka2007@gmail.com');
+
+    cy.get('[placeholder  = "Password"]').type('danilka2007{enter}');
+
+    cy.get(':nth-child(4) > .nav-link').should('contain.text', 'danilka2007')
+  }); 
 });
