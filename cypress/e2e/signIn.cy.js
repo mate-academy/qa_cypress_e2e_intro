07ca1c533eb3bc7909453d4c1954841c95ce843e
Triukhan <tryukhan.d@gmail.com>
2023-02-01 21:18:19 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,20 @@
 /// <reference types="cypress" />
 
+
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('/');
+
+    cy.contains('a', 'Sign in').should('exist').click();
     
-  });
+    cy.url().should('include', '/login');
+
+    cy.get('[placeholder  = "Email"]').type('danilka2007@gmail.com');
+
+    cy.get('[placeholder  = "Password"]').type('danilka2007{enter}');
+  
+    // OR cy.contains('.btn', 'Sign in').click();
+
+    cy.get(':nth-child(4) > .nav-link').should('contain.text', 'danilka2007')
+  }); 
 });
