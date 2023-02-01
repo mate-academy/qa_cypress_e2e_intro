cebc98290b006fd4118c82bf8ea783eb1d570a63
Yurii Yashchuk <glyposti@gmail.com>
2023-02-01 13:36:54 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,48 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+  
+  it('should contain main parts', () => {
+    cy.get('h1')
+      .should('contain.text', 'conduit');
     
+    cy.get('p')
+      .should('contain.text', 'A place to share your knowledge.')
+      
+    cy.contains('a', 'Global Feed')
+      .should('exist');
+
+    cy.contains('.sidebar', 'Popular Tags')
+      .should('exist');   
+});
+
+  it('should provide an ability to log in', () => {
+    const email = "glyposti0258@example.com";
+    const password = 'glyposti0258';
+
+    cy.contains('a', 'Sign in')
+      .should('exist')
+      .click();
+
+    cy.contains('a', 'Need an account?')
+      .should('exist');
+
+    cy.url()
+    .should('include', '/login');
+
+    cy.get('h1')
+      .should('contain.text', 'Sign In')
+
+    cy.get('[placeholder = "Email')
+      .type(email);
+
+    cy.get('[placeholder = "Password')
+      .type(password);
+
+    cy.contains('.btn', 'Sign in')
+      .click();
   });
 });
