4299fbd102b0b0a1fabb7c09915504bd8b6bd3c4
Misha Stepchuk <sandrochovych@gmail.com>
2023-02-01 23:03:59 +0100

    Solution1

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,17 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+
   it('should provide an ability to log in', () => {
-    
+
+    cy.visit('#/login')
+
+    cy.get(':nth-child(1) > .form-control').type('zero@gmail.com')
+
+    cy.get(':nth-child(2) > .form-control').type('12345')
+
+    cy.get('button').click()
+
+    cy.contains('a.nav-link','zero12').should('contain.text','zero12')
   });
 });
