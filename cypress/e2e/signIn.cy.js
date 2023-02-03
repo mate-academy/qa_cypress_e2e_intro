ab8841d0a0f4440b36e1d1b29f8ed70c5cc1da53
isid <r.bilokinskyi@gmail.com>
2023-02-03 16:18:11 +0200

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,21 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+    beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=6eu1fy')
+    })
+  
   it('should provide an ability to log in', () => {
+    cy.get('[type="email"]')
+      .type('testingcypr@gmail.com');
     
+    cy.get('[type="password"]')
+      .type('testingcypr');
+    
+    cy.get('[class="btn btn-lg btn-primary pull-xs-right"]')
+      .click();
+    
+    cy.get('[class="nav-link"]')
+      .should('contain', 'testingcypr');
   });
 });
