72e24702864e84e1f9380118dada3c78794c996d
Bobuliak Viktor <bobuliakviktor@gmail.com>
2023-02-04 19:19:20 +0200

    Cypress Inroduction

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,12 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    
+    cy.visit('https://react-redux.realworld.io');
+    cy.get(':nth-child(2) > .nav-link').click();
+    cy.get(':nth-child(1) > .form-control').type('testviktor@gmail.com');
+    cy.get(':nth-child(2) > .form-control').type('qwerty');
+    cy.get('.btn').click();
+    cy.get(':nth-child(4) > .nav-link').should('contain', 'testviktor');
+
   });
 });
