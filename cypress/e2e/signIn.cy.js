4c7eaed1d4be5273ecd7b6595218ef7415a120bc
Liliya Korniyko <kornejkolilia@gmail.com>
2023-02-06 20:45:00 +0200

    SignIn_test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -3,5 +3,17 @@
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
     
-  });
+    cy.visit ('https://react-redux.realworld.io');
+
+    cy.get(':nth-child(2) > .nav-link'). click();
+
+    cy.get(':nth-child(1) > .form-control'). type ('loto@gmail.com');
+
+    cy.get(':nth-child(2) > .form-control'). type ('pass2023');
+
+    cy.get('.btn'). click();
+
+    cy.get(':nth-child(4) > .nav-link').should ('contain.text','Loto');
+    
+   });
 });
