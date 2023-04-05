c1318aa9a8cae3ae79ca8f89a63f290e0324e6f6
Oleksandr Stryzhavchuk <sstrizhavchuk@gmail.com>
2023-04-05 11:26:11 +0200

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,21 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/login')
+  });
+
   it('should provide an ability to log in', () => {
-    
+    cy.get('[placeholder="Email"]')
+      .type('tester@team.qa')
+
+    cy.get('[placeholder="Password"]')
+      .type('Qwerty1234')
+
+    cy.contains('.btn', 'Sign in')
+      .click();
+
+    cy.get('nav')
+      .should('contain.text', 'tester1!');
   });
 });
