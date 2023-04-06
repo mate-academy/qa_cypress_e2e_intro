6f00913808fe1546957847c953201aebcdd06d54
Igor <lega.igor@gmail.com>
2023-04-06 21:46:01 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,17 @@
 /// <reference types="cypress" />
+import SignIn from "./PageObjects/signInPage";
+import Header from "./PageObjects/header";
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
-    
+describe("Sign In page", () => {
+  beforeEach(() => {
+    cy.visit("/login");
+  });
+  it("should provide an ability to log in", () => {
+    const signIn = new SignIn();
+    const header = new Header();
+    signIn.setEmail("qawsed@mail.com");
+    signIn.setPswd("1q2w3e4r");
+    signIn.submit();
+    header.checkHeaderUsername("qawsed");
   });
 });
