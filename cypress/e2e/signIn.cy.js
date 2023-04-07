d082b4c02450b349d3d10cb6f9235352ab7f9ba7
Oleksandr Ivanov <3dgeman@gmail.com>
2023-04-07 12:46:15 +0300

    cy test for sign in happy case

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,67 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
-    
+describe('Sign in page', () => {
+
+  it('should allow a user to sign in with credentials', () => {
+
+    // first we register a new user   
+
+    const randomNumber = Math.floor(Math.random() * 1000)
+    const username = `test_user${randomNumber}`
+    const email = `${username}@hotmail.com`
+    const password = 'Qwerty1!'
+
+    // sending requests is blocked in this homework :(
+    // cy.request('POST', 'api/users', {
+    //   email,
+    //   username,
+    //   password
+    // })
+    cy.visit('https://react-redux.realworld.io')
+
+    cy.contains('a', 'Sign up')
+      .click()
+
+    cy.get('[placeholder="Username"]')
+      .type(username)
+
+    cy.get('[placeholder="Email"]')
+      .type(email)
+
+    cy.get('[placeholder="Password"]')
+      .type(password)
+
+    cy.contains('.btn', 'Sign in')
+      .click()
+
+    //then we need to log out
+    cy.contains('a', 'Settings')
+      .click()
+
+    cy.contains('.btn', 'Or click here to logout.')
+      .click()
+
+    // then we sign in with created credentials
+
+    cy.contains('a', 'Sign in')
+      .click()
+
+    cy.get('[placeholder="Email"]')
+      .type(email)
+
+    cy.get('[placeholder="Password"]')
+      .type(password)
+
+    cy.contains('.btn', 'Sign in')
+      .click()
+
+    // here we do some checks to see if the user is logged in
+
+    cy.contains('a', 'Your Feed')
+      .should('exist');
+
+    cy.contains('a', username)
+      .should('exist');
+
   });
 });
