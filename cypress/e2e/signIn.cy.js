4a4b3b325f957782c68d3171343ed47cf8952bd2
Maryna Iziumova <marina.omelyanenko.96@gmail.com>
2023-04-07 11:19:23 +0200

    Conduit

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,22 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io')
+    cy.get(':nth-child(2) > .nav-link').click()
+
+    cy.get('[placeholder="Email"]')
+    .type('unicorn@gmailcom')
+
+    cy.get('[placeholder="Password"]')
+    .type('12345')
+
+    cy.get('.btn')
+    .click()
+
+    cy.wait(6000)
+
+    cy.contains('Unicorn')
+    .should('be.visible')
     
   });
 });
