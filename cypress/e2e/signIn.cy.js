468298090165a15443d65f27a29c22e212b7770a
maxymBeshleha <maxymbeshleha@gmail.com>
2023-04-07 12:20:37 +0300

    Signing in

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,11 @@
-/// <reference types="cypress" />
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    
+    cy.visit("https://react-redux.realworld.io/#/?_k=pb3kp8")
+    cy.get(':nth-child(2) > .nav-link').click()
+    cy.get(':nth-child(1) > .form-control').type("maxbeshlega228@gmail.com")
+    cy.get(':nth-child(2) > .form-control').type("max2001")
+    cy.get('.btn').click()
+    cy.get(':nth-child(4) > .nav-link').should('contain.text', 'max2001')
   });
 });
