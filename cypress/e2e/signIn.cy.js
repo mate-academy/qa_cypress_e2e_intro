4a51f71a4549f6eccffb6586864bf260f130f4fd
Olha Bazhan <olyatarasevich2@gmail.com>
2023-04-08 23:40:36 +0300

    Create first test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,22 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    
-  });
+    const randomNumber = Date.now()
+    const username = `user-${randomNumber}`
+    const email = `user-${randomNumber}@example.com`
+    const password = '121212Aa!'
+    cy.request('POST', 'https://conduit.productionready.io/api/users', {
+      "user": {
+        username,
+        email,
+        password
+      }
+    })
+    cy.visit('https://react-redux.realworld.io/')
+    cy.contains('a', 'Sign in').click()
+    cy.get('[placeholder="Email"]').type(email)
+    cy.get('[placeholder="Password"]').type(password)
+    cy.contains('button', 'Sign in').click()
+    cy.contains(':nth-child(4) > .nav-link', username)
+  })
 });
