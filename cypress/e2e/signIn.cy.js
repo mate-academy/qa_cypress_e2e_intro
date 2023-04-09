7dbc9d18a0047b5ab2122526594b6fc14e47876a
LuxuriaCode <100286381+LuxuriaCode@users.noreply.github.com>
2023-04-09 08:53:22 +0300

    Revert "Sing In - Positive test"
    This reverts commit e40ff6b3c7de20fb2e632cd1b862f2f330115771.

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,30 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-    const username = "TestUser" + Math.floor(Math.random() * 1000)
-    const email = `${username}@test.com`
-    const password = 'Test123456!'
-
-    it('should provide an ability to log in', () => {
-        cy.visit('https://react-redux.realworld.io')
-
-        cy.contains('a', 'Sign up')
-            .click()
-
-        cy.get('[placeholder="Username"]')
-            .type(username)
-
-        cy.get('[placeholder="Email"]')
-            .type(email)
-
-        cy.get('[placeholder="Password"]')
-            .type(password)
-
-        cy.contains('.btn', 'Sign in')
-            .click()
-
-        cy.contains('a', username)
-            .should('exist'); 
-
-    });
+  it('should provide an ability to log in', () => {
+    
+  });
 });
