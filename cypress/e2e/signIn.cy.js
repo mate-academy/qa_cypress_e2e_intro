d556b9e5dc5c52eef6aaee9ac1d025e91438a866
Anastasiia Stelmakh <s.anastasiia@ukr.net>
2023-04-09 13:35:57 +0300

    Stelmakh First Tests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,15 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    
+    cy.visit("https://conduit.mate.academy/")
+    cy.get(':nth-child(2) > .nav-link')
+    .click()
+    cy.get('[placeholder="Email"]')
+    .type("anastasiia_stelmakh@ukr.net")
+    cy.get('[placeholder="Password"]')
+    .type("Anastasiia_Stelmakh1")
+    cy.get('.btn')
+    .click()
+
   });
 });
