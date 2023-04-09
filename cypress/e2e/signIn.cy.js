9011f156102b0c5fe7dae9a3be3ad122b14cfac2
andrii osiiuk <osiyukandrey555@gmail.com>
2023-04-09 16:48:33 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,19 @@
 /// <reference types="cypress" />
 
+// const { use } = require("chai");
+
 describe('Sign In page', () => {
+  const email = 'o@gmail.com'
+  const password = 'asdasdasd'
+  const username = 'asd123asd123'
+  
   it('should provide an ability to log in', () => {
+    cy.visit('https://conduit.mate.academy/')
+    .get(':nth-child(2) > .nav-link').click()
+    .get('[placeholder="Email"]').type(email)
+    .get('[placeholder="Password"]').type(password)
+    .get('.btn').click()
     
+    .get('a').contains(username).should('have.text', username);
   });
 });
