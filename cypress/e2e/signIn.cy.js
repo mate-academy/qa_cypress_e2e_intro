4fe56154c0757c43d9539d0b93cb52021f6b7c0d
HalynaKomar <komarg007@gmail.com>
2023-04-09 15:07:21 +0300

    Solution e2e

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,11 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    
+    cy.visit("https://react-redux.realworld.io/#/?_k=t7rjy8")
+    cy.get(':nth-child(2) > .nav-link').click()
+    cy.get('[placeholder="Email"]').type("cypress@qa.team")
+    cy.get('[placeholder="Password"]').type("Pa$$w0rd!")
+    cy.get('.btn').click()
+    cy.get ('.navbar').should('contain.text', 'cypressTeste2e')
   });
 });
