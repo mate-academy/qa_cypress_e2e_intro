824a0c0c97c29df5b9de3a9b027fcd6c08ec262d
Oleksandra <alexandra.brightsed@gmail.com>
2023-04-09 15:45:11 +0300

    New_Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -10,7 +10,7 @@ describe('Sign In page', () => {
 
     it('should provide an ability to log in', () => {
       cy.visit('https://react-redux.realworld.io')
-  
+
       cy.contains('a', 'Sign in').click()
       cy.get('.auth-page').should('be.visible')
       cy.get('[placeholder="Email"]').should('be.visible')
@@ -19,6 +19,7 @@ describe('Sign In page', () => {
       cy.get('[placeholder="Email"]').type(email)
       cy.get('[placeholder="Password"]').type(password)
       cy.get('button[type=submit]').click()
-  
+      cy.contains('a', username).should('exist'); 
     });
   });
+
