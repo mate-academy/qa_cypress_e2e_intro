6a19a09e86784c6788a7ea344c00be56edee9d84
LuxuriaCode <100286381+LuxuriaCode@users.noreply.github.com>
2023-04-09 20:30:54 +0300

    conduit Sign In Test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,29 +1,17 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  const username = "TestUser" + Math.floor(Math.random() * 1000)
-  const email = `${username}@test.com`
-  const password = 'Test123456!'
-
   it('should provide an ability to log in', () => {
     cy.visit('https://react-redux.realworld.io')
 
-    cy.contains('a', 'Sign up')
-    .click()
+    cy.get(':nth-child(2) > .nav-link').click()
 
-    cy.get('[placeholder="Username"]')
-      .type(username)
+    cy.get('[placeholder="Email"]').type('gooz@gmail.com')
 
-    cy.get('[placeholder="Email"]')
-      .type(email)
+    cy.get('[placeholder="Password"]').type('1111')
 
-    cy.get('[placeholder="Password"]')
-      .type(password)
+    cy.get('.btn').click()
 
-    cy.contains('.btn', 'Sign in')
-      .click()
-
-      cy.contains('a', username)
-      .should('exist');
+    cy.contains('Gooz').should('be.visible')
   });
 });
