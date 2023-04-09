0705aab824e8cfe9cfdcae43a393facaeec861f3
LuxuriaCode <100286381+LuxuriaCode@users.noreply.github.com>
2023-04-09 20:47:04 +0300

    [Sing In] Positive Test updata

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,17 +1,25 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  const email = 'gooz@gmail.com';
+  const password = '1111';
+
   it('should provide an ability to log in', () => {
     cy.visit('https://react-redux.realworld.io')
 
-    cy.get(':nth-child(2) > .nav-link').click()
+    cy.get(':nth-child(2) > .nav-link')
+    .click()
 
-    cy.get('[placeholder="Email"]').type('gooz@gmail.com')
+    cy.get('[placeholder="Email"]')
+    .type(email)
 
-    cy.get('[placeholder="Password"]').type('1111')
+    cy.get('[placeholder="Password"]')
+    .type(password)
 
-    cy.get('.btn').click()
+    cy.get('.btn')
+    .click()
 
-    cy.contains('Gooz').should('be.visible')
+    cy.contains('Gooz')
+    .should('be.visible')
   });
 });
