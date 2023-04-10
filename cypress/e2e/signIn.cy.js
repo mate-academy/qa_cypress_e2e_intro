bc44f0aea48063301bbd84fa7abbef6245f1e503
Kristina Kulakivska <kristina.kuprin@gmail.com>
2023-04-10 20:53:00 +0300

    Sign In user

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,11 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    
+    cy.visit("https://react-redux.realworld.io")
+    cy.get(':nth-child(2) > .nav-link').click()
+    cy.get('[placeholder="Email"]').type("kristina.kuprin@gmail.com")
+    cy.get('[placeholder="Password"]').type("11031990")
+    cy.contains('.btn', 'Sign in').click()
+    cy.get(':nth-child(4) > .nav-link')
   });
 });
