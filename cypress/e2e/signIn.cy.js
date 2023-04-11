9c5ddaa712c92e2095fad5d5a40d7aabc038702e
Anastasiia Stelmakh <s.anastasiia@ukr.net>
2023-04-11 23:46:51 +0300

    My First Test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,15 +2,13 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    cy.visit("https://conduit.mate.academy/")
-    cy.get(':nth-child(2) > .nav-link')
-    .click()
-    cy.get('[placeholder="Email"]')
-    .type("anastasiia_stelmakh@ukr.net")
-    cy.get('[placeholder="Password"]')
-    .type("Anastasiia_Stelmakh1")
-    cy.get('.btn')
-    .click()
+    
+    cy.visit("https://react-redux.realworld.io")
 
+    cy.get(':nth-child(2) > .nav-link').click()
+    cy.get('[placeholder="Email"]').type("anastasiia_stelmakh@ukr.net")
+    cy.get('[placeholder="Password"]').type("anastasiia_stelmakh")
+    cy.get('.btn').click()
+    cy.get(':nth-child(4) > .nav-link');
   });
 });
