78fa64eae1f05e7f3cf60d1da0ba6f813287d905
Ihor Huliaiev <guliver2410@gmail.com>
2023-04-11 11:57:11 +0300

    Made changes in qa_cypress_e2e_intro

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -4,17 +4,16 @@ const password = 'IhorHuliaiev2410';
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    cy.visit('https://react-redux.realworld.io/')
-
+    cy.visit('https://react-redux.realworld.io/');
     cy.get(':nth-child(2) > .nav-link')
-    .click()
+    .click();
     cy.get('[placeholder="Email"]')
-    .type(email)
+    .type(email);
     cy.get('[placeholder="Password"]')
-    .type(password)
+    .type(password);
     cy.get('.btn')
-    .click()
+    .click();
     cy.contains('IhorHuliaiev')
-    .should('be.visible')
+    .should('be.visible');
   });
 });
