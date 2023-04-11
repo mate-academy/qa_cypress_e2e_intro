a7c0dd974d0faeefe501addde5b84ab29ab5d718
Alina Muzyka <mamajalina@gmail.com>
2023-04-11 20:54:30 +0300

    first home work cypress

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,14 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/login')
+  });
   it('should provide an ability to log in', () => {
-    
+    cy.get('[placeholder="Email"]').type('qa123456@qa.team');
+    cy.get('[placeholder="Password"]').type('qA123456');
+    cy.contains('.btn', 'Sign in').click();
+    cy.get('.article-preview').should('be.visible')
+    cy.get(':nth-child(4) > .nav-link').contains('today').should('be.visible');
   });
 });
