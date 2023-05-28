a230ec0c3632c1af630da60cf00c7dd6afd86f3a
Andrii Kuznetsov <andreykuzmich91@gmail.com>
2023-05-28 14:47:23 +0300

    cypress intro solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: '8e6nxg',
   e2e: {
     setupNodeEvents(on, config) {
     },
--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,16 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=m07odl')
+
+    cy.get('input[type="email"]').type('andreykuzmich91@qa1.com');
+
+    cy.get('input[type="password"]').type('12345678');
+
+    cy.get('button[type = "submit"]').click();
+
+    cy.contains('a', 'aaaaaaaaaaaaaaaaaaaaaaaaaa1').click();
+   
     
   });
 });
