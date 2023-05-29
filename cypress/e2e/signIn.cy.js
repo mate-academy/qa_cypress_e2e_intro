fdde4fd0da213367601007319a20462101a0932c
ann-yurkevych <linaostaphuk@gmail.com>
2023-05-29 08:03:16 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,33 @@
 /// <reference types="cypress" />
+const email = 'test1239@gmail.com';
+const password = 'Password123';
+const username = '@test1239@gmail.com';
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io')
+  });
+
+  test('should provide an ability to Sign in', () => {
+    cy.contains('.nav-link', 'Sign in')
+    .should('exist')
+    .click();
     
   });
+
+  test('should provide ability to enter password and email', () => {
+    cy.get('.form-control form-control-lg', [placeholder = "Email"])
+    .type(email);
+
+    cy.get('.form-control form-control-lg', [placeholder = "Password"])
+    .type(password);
+
+  });
+
+  test('should display the username in the header menu', () => {
+    cy.get('.nav-link')
+      .should('contain', username);
+  });
+  
 });
+
