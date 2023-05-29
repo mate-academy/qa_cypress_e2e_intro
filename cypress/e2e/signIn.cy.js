af85434095b3c092a7dc8238930d727c38f73959
Ärtem <.kosherenkov@gmail.com>
2023-05-29 13:55:35 +0300

    Solution homework Cypress Intro

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,6 +1,9 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io')
+  });
   it('should provide an ability to log in', () => {
     
   });
