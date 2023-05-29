811ba20029850b366dc2d3169359365ec67c3404
emmmcr <maximandreev99@gmail.com>
2023-05-29 13:58:31 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,15 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    
+    cy.visit('https://react-redux.realworld.io')
+    cy.get(':nth-child(2) > .nav-link')
+      .click()
+    cy.get(':nth-child(1) > .form-control')
+      .type('Testuser13@mail.com')
+    cy.get(':nth-child(2) > .form-control')
+      .type('Testuser13')
+    cy.get('.btn')
+      .click()
+    cy.contains(':nth-child(4) > .nav-link', 'Testuser13')
   });
 });
