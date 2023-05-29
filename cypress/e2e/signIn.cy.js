e20f38277f7bf96b5f73290d90bc8ec807c8e19f
Roksolana Skrypets <roksolana.skrypets.mpvpr.2021@lpnu>
2023-05-29 14:58:06 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,43 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+describe('Sign In page', () => {  
+  it('should provide an ability to log in', () => {  
+    cy.visit('https://react-redux.realworld.io');
+      const userName = 'username' + Math.random().toString().slice(2);
+      const userEmail = `${userName}@gmail.com`;
+      const userPassword = 'Qwerty123!';
     
+    cy.get(':nth-child(3) > .nav-link')
+      .click();
+    cy.get(':nth-child(1) > .form-control')
+      .type(userName);
+    cy.get(':nth-child(2) > .form-control')
+      .type(userEmail);
+
+    cy.get(':nth-child(3) > .form-control')
+      .type(userPassword);
+
+    cy.get('.btn')
+      .click();
+
+    cy.contains('.nav-link', 'Settings')
+      .click();
+
+    cy.get('.btn-outline-danger')
+      .click();
+    cy.get(':nth-child(2) > .nav-link')
+      .click();
+
+    cy.get(':nth-child(1) > .form-control')
+      .type(userEmail);
+
+    cy.get(':nth-child(2) > .form-control')
+      .type(userPassword);
+
+    cy.contains('.btn', 'Sign in')
+      .click();
+
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain.text', userName);
   });
 });
