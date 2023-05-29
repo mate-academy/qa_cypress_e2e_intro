481433e05205f4ce724cd511349fb174e5ae9861
DariaVerbovska <verbovskaya1010@gmail.com>
2023-05-30 00:21:15 +0300

    Solution_2

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,14 +1,20 @@
 /// <reference types="cypress" />
+const SIGIN_URL = 'https://react-redux.realworld.io';
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    cy.visit('https://react-redux.realworld.io/#/login');
+    cy.visit(SIGIN_URL + '/#/login');
+    
+    cy.url().should('include', '/#/login');
+
+    cy.get('h1')
+      .should('contain.text', 'Sign In');
 
     cy.get(':nth-child(1) > .form-control')
       .type('testdata.628@gmail.com');
 
     cy.get(':nth-child(2) > .form-control')
-        .type('Testdata628&@');
+      .type('Testdata628&@');
 
     cy.get('.btn')
       .click();
