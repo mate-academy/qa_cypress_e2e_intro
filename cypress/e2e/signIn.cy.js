cb64672855c683726d386be52de30bd625b8b03a
Dmytro <dmytrywork81@gmail.com>
2023-05-29 12:27:08 +0300

    Cypress HW

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,22 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
-    
+describe('Login page', () => {
+  it('should login with valid date', () => {
+    cy.visit('https://react-redux.realworld.io');
+
+    cy.get(':nth-child(2) > .nav-link')
+     .click()
+
+     cy.get(':nth-child(1) > .form-control')
+     .type('dkhozieiev@gmail.com');
+
+     cy.get(':nth-child(2) > .form-control')
+     .type('dkhozieiev');
+
+     cy.get('.btn')
+     .click()
+
+     cy.get(':nth-child(4) > .nav-link')
+     .should('contain', 'dkhozieiev');
   });
 });
