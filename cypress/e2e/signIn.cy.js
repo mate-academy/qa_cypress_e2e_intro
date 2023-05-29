0392cb09b48560efa66e323c4823555d58531742
Vitalii Kaminskyi <kaminsky0321@gmail.com>
2023-05-29 18:45:37 +0300

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,34 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
-    
+const email = 'cypressfan@gmail.com';
+const password = 'SecretFan0fCypress';
+const username = 'JustCypressFan';
+
+describe ('Sign In page', () => {
+  it('test of ability to Sign in', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io')
   });
+
+  test('test of ability to Sign in', () => {
+    cy.contains('.nav-link', 'Sign in')
+    .should('exist')
+    .click();
+
+  });
+
+  test('Test of ability to enter password and email', () => {
+    cy.get('.form-control form-control-lg', [placeholder = "Email"])
+    .type(email);
+
+    cy.get('.form-control form-control-lg', [placeholder = "Password"])
+    .type(password);
+
+  });
+
+  test('should display the username in the header menu', () => {
+    cy.get('.nav-link')
+      .should('contain', username);
+  });
+  })
 });
