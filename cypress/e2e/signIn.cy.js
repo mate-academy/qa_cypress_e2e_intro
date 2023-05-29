23cdc513004eff7ed37845b949ef1b980477006c
Danylo Kravtsov <kravtsovdania@gmail.com>
2023-05-29 16:04:36 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,16 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    
+    cy.visit('https://react-redux.realworld.io');
+    cy.contains('a', 'Sign in')
+      .click();
+    cy.get(':nth-child(1) > .form-control')
+      .type('dania@qa.team');
+    cy.get(':nth-child(2) > .form-control')
+      .type('password');
+    cy.contains('.btn', 'Sign in')
+      .click();
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', 'dania')
   });
 });
