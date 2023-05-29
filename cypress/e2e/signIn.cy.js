97a73e9dda811f7c880c297d1496e8a8fcb6f8d0
Ivan Fomenko <fomenko1ivan@gmail.com>
2023-05-29 19:49:44 +0300

    Cypress Intro

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,15 +2,23 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    cy.visit('https://react-redux.realworld.io/')
+    cy.visit({url:'https://react-redux.realworld.io/', method:'GET'})
+    
     cy.contains('Sign in')
     .click();
+    
+    cy.get('h1')
+    .should('contain.text', 'Sign In')
+    
     cy.get(':nth-child(1) > .form-control')
     .type('condtest@qa.team');
+    
     cy.get(':nth-child(2) > .form-control')
     .type('1234qwer');
+    
     cy.get('.btn')
     .click();
+    
     cy.get('.navbar')
     .should('contain', 'Ivatest');
   });
