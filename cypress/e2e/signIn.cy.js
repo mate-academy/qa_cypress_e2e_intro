98449d89fe918e7c876116e45d9c833ea0303f21
Danylo Plutenko <danielmaster010@gmail.com>
2023-05-29 14:14:47 +0300

    done

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,50 @@
 /// <reference types="cypress" />
 
+const { generateUser } = require("../support/generate");
+
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    
+    cy.visit('https://react-redux.realworld.io');
+    const {username, email, password} = generateUser();
+
+    //sign up
+    cy.get(':nth-child(3) > .nav-link')
+      .click();
+      
+    cy.get(':nth-child(1) > .form-control')
+      .type(username)
+
+    cy.get(':nth-child(2) > .form-control')
+      .type(email)
+
+    cy.get(':nth-child(3) > .form-control')
+      .type(password);
+
+    cy.contains('.btn', 'Sign in')
+      .click();  // this is not correct, because the [Sign up] button on this page doesn't exists
+    cy.wait(600);
+
+    //logout
+    cy.contains('.nav-link', 'Settings')
+      .click();
+
+    cy.get('.btn-outline-danger')
+      .click();
+
+    //sign in
+    cy.get(':nth-child(2) > .nav-link')
+      .click();
+
+    cy.get(':nth-child(1) > .form-control')
+      .type(email);
+
+    cy.get(':nth-child(2) > .form-control')
+      .type(password);
+
+    cy.contains('.btn', 'Sign in')
+      .click();
+      
+    cy.get(':nth-child(4) > .nav-link')
+      cy.should('contain.text', username)
   });
 });
