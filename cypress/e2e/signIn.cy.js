918bcd1651baf0861f4bfda690b90c748c63d48d
Ärtem <.kosherenkov@gmail.com>
2023-05-29 14:00:30 +0300

    Solution homework Cypress Intro V2

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,10 +1,23 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  const userEmail = 'test_userqwe123@gmail.com';
+  const userPassword = 'test_userqwe123';
+  const userName = 
+
   beforeEach(() => {
     cy.visit('https://react-redux.realworld.io')
   });
   it('should provide an ability to log in', () => {
-    
+    cy.contains('a', 'Sign in')
+      .click();
+    cy.get('[placeholder="Email"]')
+      .type(userEmail)
+    cy.get('[placeholder="Password"]')
+      .type(userPassword)
+    cy.contains('.btn', 'Sign in')
+      .click()
+    cy.get('.nav-link')
+      .should('contain', 'test_userqwe123');
   });
 });
