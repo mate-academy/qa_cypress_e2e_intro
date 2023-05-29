681b385f0273c3168bcb6f2896917b0d7dd26dc2
OlSkrypnikov <Al.skrypnikov@gmail.com>
2023-05-29 15:49:02 +0300

    Add automated tests for sign-in

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,52 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://react-redux.realworld.io/#/?_k=59lou9');
+
     
   });
 });
+
+describe('Sign In', () => {
+  it('should visit "Sign in" page', () => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=2yktql');
+
+    cy.contains('Sign in')
+     .click();
+  });
+});
+
+describe('Login Test', () => {
+  it('should log in successfully', () => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=2yktql');
+
+   
+    cy.get('input[type="email"]').type('xibyruqe@mailinator.com');
+    cy.get('input[type="password"]').type('Qwerty1!');
+
+  
+    cy.contains('button', 'Sign in').click();
+    
+    
+
+  });
+  });
+  
+  describe('Visible Name in header', () => {
+    it('Visible name in header', () => {
+      cy.visit('https://react-redux.realworld.io/#/login?_k=2yktql');
+  
+     
+      cy.get('input[type="email"]').type('xibyruqe@mailinator.com');
+      cy.get('input[type="password"]').type('Qwerty1!');
+  
+    
+      cy.contains('button', 'Sign in').click();
+      
+      cy.contains('.nav-link', 'PeterQuill')
+  
+      
+    });
+});
+
