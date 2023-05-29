da12f792b214d32f9d52e45f2483122d3a2ddb9c
anna-lymorenko <anyalymorenko@gmail.com>
2023-05-29 20:19:12 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -4,6 +4,12 @@ describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
     cy.visit('https://react-redux.realworld.io/#/login?_k=vsrq3e');
 
+    cy.url()
+      .should('include', '/login');
+
+    cy.get('h1')
+      .should('contain.text', 'Sign In');
+
     cy.get('input[type="email"]')
       .type('anyalymorenko@gmail.com');
 
@@ -12,8 +18,8 @@ describe('Sign In page', () => {
 
     cy.contains('.btn', 'Sign in')
       .click();
-
-    cy.contains('.nav-link', 'anyalymo')
-      .click();
+     
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', 'anyalymo');
   });
 });
