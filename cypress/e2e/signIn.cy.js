e56aba23f428d6b892707d8378e6a4c487704490
Yuliia Vereshko <pyliatko@gmail.com>
2023-05-29 14:49:13 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,19 @@
 /// <reference types="cypress" />
 
+
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    
+    const username = 'vesna23';
+    const email = 'vesna23@gmail.com';
+    const password = 'Vesna2023';
+    cy.visit('/#/login')
+    cy.get('[placeholder="Email"]')
+      .type(email)
+    cy.get('[placeholder="Password"]')
+      .type(password);
+    cy.contains('.btn', 'Sign in')
+      .click();
+    cy.get('.navbar')
+      .should('contain', username);
+  })
   });
-});
