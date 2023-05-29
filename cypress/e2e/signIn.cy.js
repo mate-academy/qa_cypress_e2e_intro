6bbaba603f090464cc420b877428372efe320783
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2023-05-29 14:37:13 +0300

    Add automated test for Sign In flow

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,15 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    
+    cy.visit('https://react-redux.realworld.io/#/login');
+
+    cy.get(':nth-child(1) > .form-control')
+    .type('yuliiaPP@qa.team');
+    cy.get(':nth-child(2) > .form-control')
+      .type('12345');
+    cy.get('.btn')
+      .click();
+
+    cy.get(':nth-child(4) > .nav-link').should('contain', 'YuliiaP');
   });
 });
