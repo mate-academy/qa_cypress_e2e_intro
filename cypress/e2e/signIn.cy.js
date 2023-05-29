1caff5c0da6d00dbd2fa18bbbb51ccf73cd5f770
Your GitHub Username <Your GitHub Email>
2023-05-29 19:36:13 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,22 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+  
+    cy.visit('https://react-redux.realworld.io/')
     
+    cy.contains('Sign in')
+      .click()
+    
+    cy.get('[placeholder="Email"]')
+      .type('plrtuwi@qa.team')
+
+    cy.get('[placeholder="Password"]')
+      .type('hEllo123#')
+
+    cy.contains('.btn', 'Sign in')
+      .click()
+
+    cy.get(':nth-child(4) > .nav-link')
+     .should('contain', 'plrtuwi');
   });
 });
