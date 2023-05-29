543eb230059274bd93eed08e5fc74a13f6d97135
YuryTatarko <hatemaders@gmail.com>
2023-05-29 19:47:11 +0300

    Add automated tests for sign-in

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -4,6 +4,11 @@ describe('Sign In', () => {
   it('should visit "Sign in" page and log in successfully', () => {
     cy.visit('https://react-redux.realworld.io/#/login?_k=2yktql');
 
+    // Check "Sign in" page
+    cy.contains('h1', 'Sign In').should('be.visible');
+    cy.url().should('include', '/login');
+    cy.url().should('include', 'login');
+
     // Fill in email and password fields
     cy.get('input[type="email"]').type('matzger@gmail.com');
     cy.get('input[type="password"]').type('Me771835');
@@ -12,6 +17,6 @@ describe('Sign In', () => {
     cy.contains('button', 'Sign in').click();
 
     // Assert that the username appears in the site navigation menu
-    cy.contains('.nav-link', 'Matzger');
+    cy.contains('.nav-link', 'Matzger').should('be.visible');
   });
 });
