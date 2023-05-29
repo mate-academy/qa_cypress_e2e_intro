7ad00f8c3998af582b44632df3eef4d73604769d
Artem <protiv55@gmail.com>
2023-05-29 22:40:22 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,18 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io/#/login');
+
+    cy.get(':nth-child(1) > .form-control')
+      .type('artq1234@mail.com');
     
+    cy.get(':nth-child(2) > .form-control')
+      .type('Artq1234');
+
+    cy.get('.btn')
+        .click();
+      
+    cy.get(':nth-child(4) > .nav-link')
+      .contains('Qui sunt distinctio');
   });
 });
