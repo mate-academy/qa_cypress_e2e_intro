874b96b5841c4cfa46241c693c79c5b7d93b8dfa
Kyrylo Aleinykov <kyrryloaleynikov@gmail.com>
2023-05-29 16:04:56 +0300

    First e2e test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,19 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/login')
+  });
+
   it('should provide an ability to log in', () => {
+    cy.get(':nth-child(1) > .form-control')
+    .type('ghostmane@qa.team')
     
+
+    cy.get(':nth-child(2) > .form-control')
+    .type('12345');
+
+    cy.contains('.btn', 'Sign in')
+    .click();
   });
 });
