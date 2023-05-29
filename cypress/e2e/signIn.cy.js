d840dc579c723b313dbb7b7fc38d49bf4342d202
YuryTatarko <hatemaders@gmail.com>
2023-05-29 17:56:39 +0300

    Add automated tests for sign-in

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,24 +1,7 @@
 /// <reference types="cypress" />
 
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://react-redux.realworld.io/#/?_k=59lou9');
-
-    
-  });
-});
-
 describe('Sign In', () => {
-  it('should visit "Sign in" page', () => {
-    cy.visit('https://react-redux.realworld.io/#/login?_k=2yktql');
-
-    cy.contains('Sign in')
-     .click();
-  });
-});
-
-describe('Login Test', () => {
-  it('should log in successfully', () => {
+  it('should visit "Sign in" page and log in successfully', () => {
     cy.visit('https://react-redux.realworld.io/#/login?_k=2yktql');
 
     // Fill in email and password fields
@@ -28,22 +11,7 @@ describe('Login Test', () => {
     // Click on Sign In button
     cy.contains('button', 'Sign in').click();
 
-    
-  });
-});
-
-describe('Login Test', () => {
-  it('should log in successfully', () => {
-    cy.visit('https://react-redux.realworld.io/#/login?_k=2yktql');
-
-    // Fill in email and password fields
-    cy.get('input[type="email"]').type('matzger@gmail.com');
-    cy.get('input[type="password"]').type('Me771835');
-
-    // Click on Sign In button
-    cy.contains('button', 'Sign in').click();
-    cy.contains('.nav-link', 'Matzger')
-
-    
+    // Assert that the username appears in the site navigation menu
+    cy.contains('.nav-link', 'Matzger');
   });
 });
