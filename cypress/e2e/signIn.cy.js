459ca78574f9042d63c426076edc342234d11cc7
stanyslav-petryk <petrikstasfb@gmail.com>
2023-05-29 16:10:03 +0400

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,14 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    
+    cy.visit('https://react-redux.realworld.io/#/login?_k=m07odl')
+
+    cy.get('input[type="email"]').type('rgmregremmw@gmail.com');
+
+    cy.get('input[type="password"]').type('88888888');
+
+    cy.get('button[type = "submit"]').click();
+
+    cy.contains('a', 'grrmegpregm').click();
   });
 });
