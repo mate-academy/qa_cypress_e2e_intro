685d18af35ce74b818a16d1cd7281a4c48e3f7e0
OlehHoryk <skarllet8@gmail.com>
2023-05-29 17:40:06 +0300

    oleh333

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,14 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    
+    cy.visit('https://react-redux.realworld.io/#/login')
+    cy.get(':nth-child(1) > .form-control')
+    .type('oleh333@gmail.com')
+    cy.get(':nth-child(2) > .form-control')
+    .type('oleh333')
+    cy.get('.btn')
+    .click()
+    cy.get('.navbar')
+    .should('contain', 'oleh333')
   });
 });
