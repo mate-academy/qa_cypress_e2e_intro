41362657531a9bc97cb83008603e8651161bab99
Roman Vytrykush <qwertywo2000@gmail.com>
2023-05-29 16:03:33 +0300

    test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,26 @@
 /// <reference types="cypress" />
 
+const baseUrl = 'https://react-redux.realworld.io';
+
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit(baseUrl);
+
+    cy.contains('Sign in')
+    .click();
     
-  });
+    cy.get('[type = email]')
+    .type('123qwe@mail.com');
+
+    cy.get('[type = password]')
+    .type('passpass');
+
+    cy.get('[type = submit]')
+    .click();
+    
+    cy.get('[class = "nav-link active"]')
+    .should('contains.text', 'Your feed');
+  }
+  
+  );
 });
