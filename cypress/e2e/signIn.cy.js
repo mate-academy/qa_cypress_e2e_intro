63ca4c17d17701060e032c3cabfcad48f9768afc
Yaroslvna Horban <domkomp2709@gmail.com>
2023-05-29 19:05:39 +0200

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,27 @@
 /// <reference types="cypress" />
 
+const email = 'cypresstest@qa.com';
+const password = 'cypresstestqa';
+const username = 'Test Username';
+
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+  
+    cy.visit('https://react-redux.realworld.io/')
     
+    cy.contains('Sign in')
+      .click()
+    
+    cy.get('[placeholder="Email"]')
+      .type(email)
+
+    cy.get('[placeholder="Password"]')
+      .type(password)
+
+    cy.contains('.btn', 'Sign in')
+      .click()
+
+    cy.get(':nth-child(4) > .nav-link')
+     .should('contain', username);
   });
 });
