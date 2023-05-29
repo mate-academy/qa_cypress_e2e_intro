d0c70e5d81d51b5fa76497eb3c4c2aa94816e6f7
BakumSV <immigrantsong61@gmail.com>
2023-05-29 17:30:00 +0300

    Task is resolved

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,28 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  beforeEach (() => {
+    cy.visit('/')
+  });
+
   it('should provide an ability to log in', () => {
+    const username = 'Customer426';
+    const email = 'customer426@mail.com';
+    const password = 'Password426';
+
+    cy.get(':nth-child(2) > .nav-link')
+      .click()
+
+    cy.get('[placeholder="Email"]')
+      .type(email)
     
+    cy.get('[placeholder="Password"]')
+      .type(password)
+
+    cy.contains('.btn', 'Sign in')
+      .click()
+    
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', username)
   });
 });
