7e76ea4a7b38426289d48531712d61055a74fda0
Denys Shyshlakov <444.spam@gmail.com>
2023-05-29 17:38:44 +0300

    First Cypress test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,25 @@
 /// <reference types="cypress" />
+const random = Math.floor(Math.random (1000) * 1000);
+const userName = `user_den${random}`;
+const email = `${userName}@gmail.com`;
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
-    
+describe('Registration', () => {
+  before(() => {
+    cy.visit('https://react-redux.realworld.io');
+  });
+
+  it('Register a new user', () => {
+    cy.get(':nth-child(3) > .nav-link')
+    .click ();
+    cy.get(':nth-child(1) > .form-control')  
+    .type (userName);
+    cy.get(':nth-child(2) > .form-control')
+    .type (email);
+    cy.get(':nth-child(3) > .form-control')
+    .type("Qwerty!23");
+    cy.get('.btn')
+    .click();
+    cy.get(':nth-child(4) > .nav-link')
+    .should('contain',userName)
   });
 });
