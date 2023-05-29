84630c1eb664c2337a32f41a955ae4b0d56be44d
Iia Hnatiuk <iyuschaaa@gmail.com>
2023-05-29 17:54:14 +0300

    Solution IIA

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,21 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  before(() => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=7g16bq');
+  });
+
   it('should provide an ability to log in', () => {
+    cy.get(':nth-child(1) > .form-control')
+      .type('Anna_Maria1234@mail.com');
+
+    cy.get(':nth-child(2) > .form-control')
+      .type('Anna_Maria1234');
     
+    cy.contains('.btn', 'Sign in')
+      .click();
+     
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', 'Anna_Maria1234')
   });
 });
