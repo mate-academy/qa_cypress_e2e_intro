4bded03b1d5a020419ed55cce6fb7e7d3759ee2d
Oleksandr Ohryzko <sasha.ogrizko@gmail.com>
2023-05-29 12:28:26 +0300

    Tests for sign in

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,21 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io')
     
+    cy.get(':nth-child(2) > .nav-link')
+      .click();
+
+    cy.get(':nth-child(1) > .form-control')
+      .type('test_user_321@gmail.com');
+
+    cy.get(':nth-child(2) > .form-control')
+      .type('12345678');
+
+    cy.contains('.btn', 'Sign in')
+     .click();
+
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', 'test_user_321');
   });
 });
