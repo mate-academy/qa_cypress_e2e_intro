c79f9781ba6afda359daecb425c3f8caf0c2bf1e
Iryna_Beznoshchenko <irabeznoshchenko@gmail.com>
2023-05-29 20:06:02 +0300

    Solution after changes

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -3,7 +3,8 @@
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
     cy.visit('https://react-redux.realworld.io');
-    cy.get(':nth-child(2) > .nav-link ').click();
+    cy.contains('.nav-link', 'Sign in')
+    .click();
     cy.get('[placeholder="Email"]').type('irina@test.qa');
     cy.get('[placeholder="Password"]').type('qwerty');
     cy.get('.btn').click();
