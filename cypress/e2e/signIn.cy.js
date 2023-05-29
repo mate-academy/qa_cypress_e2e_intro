e2fa194d0fe079a8de3965abccf30243075cff56
anna-lymorenko <anyalymorenko@gmail.com>
2023-05-29 17:05:18 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,18 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    
+    cy.visit('https://react-redux.realworld.io/#/login?_k=vsrq3e');
+
+    cy.get('input[type="email"]')
+      .type('anyalymorenko@gmail.com');
+
+    cy.get('input[type="password"]')
+      .type('12345678');
+
+    cy.contains('.btn', 'Sign in')
+      .click();
+
+    cy.contains('.nav-link', 'anyalymo')
+      .click();
   });
 });
