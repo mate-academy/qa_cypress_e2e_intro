27f421d5d3817435bdb6ee837280db364aa0e910
Iryna <novydom23@gmail.com>
2023-05-30 13:07:01 +0300

    Test add Sign In

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -8,7 +8,8 @@ describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
     cy.get(':nth-child(2) > .nav-link')
    .click();
-   cy.url('/login');
+   cy.url() .should('include','/login');
+
    cy.get(':nth-child(1) > .form-control')
    .type('test36@gmail.com');
    cy.get(':nth-child(2) > .form-control')
