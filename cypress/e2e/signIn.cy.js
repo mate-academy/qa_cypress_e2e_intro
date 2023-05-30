83ebcae1fec4685484537fae6f210f23223009ed
Maria Dusyk <fibrmirr@gmail.com>
2023-05-30 17:27:38 +0300

    First auto-test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,19 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
-    
+
+describe('Sign In', () => {
+  it('should visit "Sign in" page and log in successfully', () => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=2yktql');
+
+    // Fill in email and password fields
+    cy.get('input[type="email"]').type('urinary12@gmail.com');
+    cy.get('input[type="password"]').type('Qwerty123456');
+
+    // Click on Sign In button
+    cy.contains('button', 'Sign in').click();
+
+    // Assert that the sername appears in the site navigation menu
+    cy.contains('.nav-link', 'urinary12');
   });
 });
+  
