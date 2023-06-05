68a801a985ae2b228463b465187d846af6290de2
Anastasiia Zheliaskova <a.zheliaskova3@gmail.com>
2023-06-05 16:28:56 +0300

    Solution_2

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -8,7 +8,7 @@ describe('Sign In page', () => {
   beforeEach(() => {
     cy.visit('https://react-redux.realworld.io/');
   });
-
+  // it works
   it('should provide an ability to log in', () => {
     cy.contains('.nav-link', 'Sign in')
       .click();
