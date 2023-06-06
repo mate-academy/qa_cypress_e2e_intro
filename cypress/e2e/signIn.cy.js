7a080415e52e98d6721254bee3f1a48fbc41dc1a
Dima Cherednyk <dicherednyk@gmail.com>
2023-06-06 19:04:40 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,32 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  const user = 'user-test12345';
+  const email = 'user-test12345@gmail.com';
+  const password = 'user12345';
 
+  it('should provide an ability to log in', () => {
+    cy.visit('/');
+
+    cy.contains('a', 'Sign in')
+      .click();
+
+    cy.url()
+      .should('include', '/login');
+
+    cy.get('h1')
+      .should('contain.text', 'Sign In');
+
+    cy.get('[placeholder="Email"]')
+      .type(email);
+
+    cy.get('[placeholder="Password"]')
+      .type(password);
+
+    cy.contains('button', 'Sign in')
+      .click();
+
+    cy.contains('.nav-link', user)
+      .should('exist');
   });
 });
