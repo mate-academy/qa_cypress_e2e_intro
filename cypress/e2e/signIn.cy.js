d1cbcfd52fd01cf07b67f004a09ded2d235a6c47
Bohdan <sventuhcorp@gmail.com>
2023-07-04 18:00:09 +0300

    the first test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,15 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
+describe('Sign In page', () => {  
   it('should provide an ability to log in', () => {
-
+    cy.visit('https://react-redux.realworld.io/#/login')
+    cy.get('[type="email"]')
+      .type('nomatterwhat@test.com');
+    cy.get('[type="password"]')
+      .type('nomatterwhat');
+    cy.get('.btn')
+       .click();
+    cy.get('.nav-link')
+       .should('contain.text', 'Nomatterwhat');
   });
 });
