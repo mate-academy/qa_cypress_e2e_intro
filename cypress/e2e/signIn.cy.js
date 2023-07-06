22c06767df60918f3f7d33a266568a3b44bb7801
Alina Tkachuk <alina.tkachuk.qa@gmail.com>
2023-07-06 12:28:03 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,32 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
 
-  });
+beforeEach(()=> {
+  cy.visit ('/')
+})
+
+it('should provide an ability to log in', () => {
+  cy.contains('a', 'Sign in')
+    .should('exist')
+    .click();
+
+  cy.url()
+    .should('include', '/login');
+
+  cy.get('h1')
+    .should('contain.text', 'Sign In');
+
+  cy.get(':nth-child(1) > .form-control')
+    .type('afgghhb@mailinator.com');
+
+  cy.get(':nth-child(2) > .form-control')
+    .type('Pa$$w0rd!');
+
+  cy.contains('.btn', 'Sign in')
+    .click();
+
+  cy.get('a')
+    .should('contain.text', 'dddjddd');
 });
+   });
