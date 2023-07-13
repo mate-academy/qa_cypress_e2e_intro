a267100edf232e8683e6e45782c2b8e2f0d76bde
OleksandraLevytska01 <alexandralevytska@gmail.com>
2023-07-13 16:50:14 +0300

    Solution11

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,15 +2,16 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-    const userName = 'Alex1205@gmail.com';
-    const password = 'kassandra01';
+    const email = 'user1408@gmail.com';
+    const password = 'user1205';
+    const userName = 'user1408';
 
     cy.visit('https://react-redux.realworld.io');
 
     cy.get(':nth-child(2) > .nav-link')
       .click();
     cy.get(':nth-child(1) > .form-control')
-      .type(userName);
+      .type(email);
     cy.get(':nth-child(2) > .form-control')
       .type(password);
     cy.get('.btn')
