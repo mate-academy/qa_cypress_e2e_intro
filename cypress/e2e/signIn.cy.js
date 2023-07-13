f2290a4e3d737ba8f58f5b2d9f664375123c5665
Tolik Serhiychuk <sergiytchoukk@gmail.com>
2023-07-13 17:38:11 +0300

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,22 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    const email = 'UserQA@gmail.com';
+    const password = 'Qwerty1234';
+    const username = 'UserQA';
 
+    cy.visit('/');
+
+    cy.get(':nth-child(2) > .nav-link')
+      .click();
+
+    cy.get(':nth-child(1) > .form-control')
+      .type(email);
+    cy.get(':nth-child(2) > .form-control')
+      .type(password);
+    cy.get('.btn')
+      .click();
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', username);
   });
 });
