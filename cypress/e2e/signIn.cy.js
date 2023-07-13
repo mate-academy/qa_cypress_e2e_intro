e1ba342bbc72cebaa634c8427d2832910b5617af
Olena Lapa <olena.lapa@gmail.com>
2023-07-13 17:08:59 +0300

    new changes

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,12 @@
-/// <reference types="cypress" />
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io');
+    cy.get(':nth-child(2) > .nav-link').click();
 
+    cy.get(':nth-child(1) > .form-control').type('wedilo2989@lukaat.com');
+    cy.get(':nth-child(2) > .form-control').type('4V6VyG4b3u');
+    cy.get('.btn').click();
+    cy.get(':nth-child(4) > .nav-link').should('be.visible');
   });
 });
