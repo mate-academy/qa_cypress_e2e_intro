c0526dd8dcff1b2870e99edfca9602e68dbb81c4
Roman Miskiv <molfargoods@gmail.com>
2023-07-13 14:46:30 +0300

    Implemented Cypress tests for sign in

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,18 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+describe('Sign In Test', () => {
+  it('should sign in successfully and verify username', () => {
+    // Visit "Sign in" page
+    cy.visit('https://react-redux.realworld.io/#/login');
 
+    // Fill in email and password fields with valid data
+    cy.get('input[type="email"]').type('shakirashakira11@qa.team');
+    cy.get('input[type="password"]').type('qwerty12345');
+
+    // Click on "Sign In" button
+    cy.contains('button', 'Sign in').click();
+
+    // Assert that username appears in the "Site navigation (Header)" menu
+    cy.get('.nav-link').should('contain', 'Shakirashakira');
   });
 });
