acf90ca766c9b73fce892dc153925beba695ca88
zhabchyk.anastasia@gmail.com <zhabchyk.anastasia@gmail.com>
2023-07-13 14:56:46 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,20 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io/');
+    cy.get(':nth-child(2) > .nav-link')
+      .click();
 
+    cy.get(':nth-child(1) > .form-control')
+      .type('AnnaSmith@mail.com');
+
+    cy.get(':nth-child(2) > .form-control')
+      .type('1q2w3e4r5t');
+
+    cy.get('.btn')
+      .click();
+
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', 'AnnaSmith');
   });
 });
