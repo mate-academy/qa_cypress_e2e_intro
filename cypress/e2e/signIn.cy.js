da3e2f8b5e2ba025ad192c2deb3872eefbae0d68
Valerij Kozyrenko <devilofwater777@gmail.com>
2023-07-13 19:00:45 +0300

    Solution5

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,33 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/login');
+  });
 
+  it('should provide an ability to log in', () => {
+    const userName = `matesobaka@mate.ua`;
+    const Password = `123123123`;
+
+    cy.url()
+      .should('include', '/login');
+    cy.get('h1.text-xs-center')
+      .should(`be.visible`);
+
+    cy.get('h1.text-xs-center')
+      .should('contain.text', 'Sign In');
+    cy.get(':nth-child(1) > .form-control')
+      .type(userName);
+    cy.get(':nth-child(2) > .form-control')
+      .type(Password);
+    cy.get('.btn')
+      .click();
+
+    cy.url()
+      .should('include', '/login');
+    cy.get('.btn')
+      .should(`be.visible`);
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain.text', 'user222222');
   });
 });
