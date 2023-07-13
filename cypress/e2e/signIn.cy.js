9f786758ca9a279e4e88c574af04f8ec902c9f11
Lapin Vlad <alalon2@icloud.com>
2023-07-13 18:58:13 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,28 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=nh2o37');
+  });
 
+  it('should provide an ability to log in', () => {
+    const Email = 'Usertest1@test.com';
+    const Password = 'Password1';
+
+    cy.url()
+      .should('include', '/login');
+    cy.get('h1.text-xs-center')
+      .should('contain.text', 'Sign In');
+    cy.get(':nth-child(1) > .form-control')
+      .type(Email);
+    cy.get(':nth-child(2) > .form-control')
+      .type(Password);
+    cy.get('.btn')
+      .click();
+
+    cy.url()
+      .should('include', '/login');
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain.text', 'User_test1');
   });
 });
