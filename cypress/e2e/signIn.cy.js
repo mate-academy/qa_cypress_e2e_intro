1f7211275eda3d9c3deb258d06ef756d43ed943e
OleksandraLevytska01 <alexandralevytska@gmail.com>
2023-07-13 12:19:09 +0300

    cypress/e2e/signIn.cy.js

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,19 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    const userName = 'Alex1205@gmail.com';
+    const password = 'kassandra01';
 
+    cy.visit("https://react-redux.realworld.io")
+
+    cy.get(':nth-child(2) > .nav-link')
+      .click();
+    cy.get(':nth-child(1) > .form-control')
+      .type(userName)
+    cy.get(':nth-child(2) > .form-control')
+      .type(password)
+    cy.get('.btn')
+      .click()
+    cy.get(':nth-child(4) > .nav-link').should('contain', userName)
   });
 });
