25482cbbb477cc8f990bf2e8390f1077ba372ce2
Denys Mandebura <mandebura1802@gmail.com>
2023-07-13 14:38:15 +0300

    changed autotest

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,20 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io');
 
+    cy.get(':nth-child(2) > .nav-link')
+      .click();
+
+    cy.get(':nth-child(1) > .form-control')
+      .type('sigma@gmail.com');
+    cy.get(':nth-child(2) > .form-control')
+      .type('Latinoheat1!');
+
+    cy.get('.btn')
+      .click();
+
+    cy.get(':nth-child(4) > .nav-link').should('be.visible');
+    cy.get(':nth-child(4) > .nav-link').should('have.text', 'joebin1');
   });
 });
