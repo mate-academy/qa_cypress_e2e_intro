2f8c7889cf46e5040e73f0bf416b32ea0a7bae65
Anna Kachurets <avk16031993@gmail.com>
2023-07-25 12:01:22 +0300

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,22 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('/');
+
+    cy.get(':nth-child(2) > .nav-link')
+      .click();
+
+    cy.get(':nth-child(1) > .form-control')
+      .type('anna_anna111@mail.com');
+
+    cy.get(':nth-child(2) > .form-control')
+      .type('anna_anna111');
+
+    cy.get('.btn')
+      .click();
+
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain.text', 'anna_anna111');
 
   });
 });
