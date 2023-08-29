592e7d2ae2255b4bb1ed4ebdfbbb2c8ad4341cad
Olesia Semen <semenolesa01@gmail.com>
2023-08-29 14:53:55 +0300

    Testing3

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -5,18 +5,15 @@ describe('Sign In page', () => {
 it('should provide an ability to log in', () => {
  const randomNumber = Math.random().toString().slice(2);
  const userName = 'test_user_${randomNumber}';
-it('should click on Sign In',() => {
 cy.contains('a', 'Sign in').click();
 cy.url().should('include','/login');
 cy.get('h1').should('contain.text', 'Sign In');
 cy.get(':nth-child(1) > .form-control')
- .type('userNamet@gmail.com');
+ .type('userName@gmail.com');
  cy.get(':nth-child(2) > .form-control')
  .type('test1234');
  cy.get('.btn').click();
- cy.get('swal-modal')
- .should('contain.text', 'userName' );
+ cy.get(':nth-child(2) > .nav-link')
+});
+});
 
-});
-});
-});
