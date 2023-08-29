a77c5057111d4f140bd2d93e8a8d5a076dd47e1f
Olesia Semen <semenolesa01@gmail.com>
2023-08-29 12:38:43 +0300

    Testing2

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,7 +2,7 @@ describe('Sign In page', () => {
   beforeEach(() => {
  cy.visit('https://react-redux.realworld.io')
 });
-it('should login user', () => {
+it('should provide an ability to log in', () => {
  const randomNumber = Math.random().toString().slice(2);
  const userName = 'test_user_${randomNumber}';
 it('should click on Sign In',() => {
@@ -13,9 +13,10 @@ cy.get(':nth-child(1) > .form-control')
  .type('userNamet@gmail.com');
  cy.get(':nth-child(2) > .form-control')
  .type('test1234');
- cy.get('.btn');
+ cy.get('.btn').click();
  cy.get('swal-modal')
  .should('contain.text', 'userName' );
+
 });
 });
 });
