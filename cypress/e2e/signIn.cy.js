93296033bb2a91c6748c7c9e034c360ac73c6554
Olesia Semen <semenolesa01@gmail.com>
2023-09-02 22:37:36 +0300

    Testing5

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,16 +1,17 @@
 /// <reference types="cypress" />
+
 describe('Sign In page', () => {
-  it('should login user', () => {
- cy.visit('https://react-redux.realworld.io')
-});
-
-
- cy.contains('a', 'Sign in').click();
+it('should login user', () => {
+ cy.visit('https://react-redux.realworld.io');
+ const validEmail = 'userNam@gmail.com';
+ const validPassword = 'test1234';
+ const validUserName = 'usernam123';
+ cy.contains('a', 'Sign in').click(); 
  cy.get(':nth-child(1) > .form-control')
    .type('userNam@gmail.com');
    cy.get(':nth-child(2) > .form-control')
    .type('test1234');
    cy.get('.btn').click();
-   cy.get('.navbar')
-   .should('contain.text', usernam123 );
+   cy.get(':nth-child(4) > .nav-link');
+});
 });
