a56500cedee95748e70ff8098164f546514688d2
Yelyzaveta <filimonova.elizabeth.a@gmail.com>
2023-09-13 13:47:32 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,16 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  const email = 'qayf@test.com';
 
+  it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io');
+    cy.get(':nth-child(2) > .nav-link');
+    cy.contains('Sign in').click();
+    cy.get(':nth-child(1) > .form-control')
+      .type(email);
+    cy.get(':nth-child(2) > .form-control')
+      .type('test1234');
+    cy.get('.btn').click();
   });
 });
