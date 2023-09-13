a12be486521d95582fc53c41ccf864aac602318b
Olesia Rozkolupa <olesiarozko1234@gmail.com>
2023-09-13 12:58:17 +0300

    Solution4

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,17 +2,14 @@
 
 describe('Site page', () => {
   beforeEach(() => {
-    cy.visit('https://react-redux.realworld.io/#/login?_k=lrfhka');
+    cy.visit('https://react-redux.realworld.io');
   });
 
-  it('should open "Sign in" page', () => {
-    cy.get('h1').should('contain.text', 'Sign In');
-  });
+  cy.get('h1').should('contain.text', 'Sign In');
 
-  it('should click on "Sign in" link in the navigation', () => {
-    cy.contains('a', 'Sign in').should('exist').click();
-    cy.get('h1').should('contain.text', 'Sign In');
-  });
+  cy.contains('Sign in').click();
+
+  cy.get('h1').should('contain.text', 'Sign In');
 
   it('should fill in email and password fields with valid data', () => {
     cy.get('[placeholder=Email]')
@@ -21,10 +18,10 @@ describe('Site page', () => {
     cy.get('[placeholder=Password]')
       .type('Oleksa19*');
 
-    it('should click on "Sign in" link ', () => {
-      cy.contains('a', 'Sign in').should('exist').click();
-      cy.get('h1').should('contain.text', 'Sign In');
+    it('should click on [Sign in] button ', () => {
+      cy.get('.btn').click();
     });
+
     cy.get('nav').contains('OleksaD19').should('exist');
   });
 });
