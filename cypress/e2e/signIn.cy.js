a7ef1d39ae2ee1dda22afe0198e67a0cde73cd7d
Olha <ogvozdeckas@gmail.com>
2023-09-13 12:52:36 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('Checks user flow in Sign In page', () => {
+  it('should provide an ability to log in', () => {
     cy.visit('https://react-redux.realworld.io');
 
     cy.get(':nth-child(2) > .nav-link').click();
