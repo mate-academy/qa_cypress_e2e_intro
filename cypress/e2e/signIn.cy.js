e905e9607ee85b6b38ef1bb3195f6ce0a87d4d10
Olena Krytovych <olena.krytovych@gmail.com>
2023-09-13 14:04:35 +0300

    Sign in test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,19 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io')
+
+    cy.get(':nth-child(2) > .nav-link').click();
+
+    cy.get(':nth-child(1) > .form-control').type('olenka@mail.com');
+
+    cy.get(':nth-child(2) > .form-control').type('Testing1!');
+
+    cy.get('.btn').click();
+
+    cy.get(':nth-child(4) > .nav-link').should('contain', 'Olena123');
+
+
 
   });
 });
