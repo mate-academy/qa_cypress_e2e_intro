86ae6bbcced0c0472d7012883452578a6528df0c
PolinaAlieksieieva <polinasetrakova408@gmail.com>
2023-09-13 16:22:35 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,16 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io');
 
+    cy.get(':nth-child(2) > .nav-link').click();
+
+    cy.get(':nth-child(1) > .form-control').type('malinator2345@gmail.com');
+
+    cy.get(':nth-child(2) > .form-control').type('QApolina2223@@');
+
+    cy.get('.btn').click();
+
+    cy.get(':nth-child(4) > .nav-link').should('contain', 'surround123');
   });
 });
