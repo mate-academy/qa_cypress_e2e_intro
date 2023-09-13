98012e22926cd5aac1c85dee03a604fc921eba85
Yelyzaveta <122609036+YelyzavetaFilimonova@users.noreply.github.com>
2023-09-13 14:08:56 +0300

    SignIn.cy.js

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -5,12 +5,13 @@ describe('Sign In page', () => {
 
   it('should provide an ability to log in', () => {
     cy.visit('https://react-redux.realworld.io');
-    cy.get(':nth-child(2) > .nav-link');
-    cy.contains('Sign in').click();
+    cy.get(':nth-child(2) > .nav-link').contains('Sign in').click();
     cy.get(':nth-child(1) > .form-control')
       .type(email);
     cy.get(':nth-child(2) > .form-control')
       .type('test1234');
     cy.get('.btn').click();
+    cy.get(':nth-child(4) > .nav-link')
+    .should('contain.text', 'qayf');
   });
 });
