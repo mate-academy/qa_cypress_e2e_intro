ad88805a0c0d42db9b81c3b4a1c6aeab0406d585
mike <mykolaiipobihailo@gmail.com>
2023-09-13 08:46:37 +0300

    Cypress-into-solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,28 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io');
+  });
 
+  it('should click on Sign In link', () => {
+    cy.contains('a', 'Sign in').should('exist').click();
+
+    cy.get('h1').should('contain.text', 'Sign In');
+  });
+
+  it('should provide an ability to log in', () => {
+    const email = 'naxu@mailinator.com';
+    const password = 'Pa$$word123';
+
+    cy.contains('Sign in').click();
+
+    cy.get('h1').should('contain.text', 'Sign In');
+
+    cy.get('[placeholder=Email]').type(email);
+
+    cy.get('[placeholder=Password]').type(password);
+
+    cy.get('.btn').click();
   });
 });
