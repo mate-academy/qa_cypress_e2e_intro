722865e1469f10e64ad535d7fb42cf99aa414165
VetKochetkov <bet4mail@gmail.com>
2023-09-13 11:50:40 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,14 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io/#login');
 
+    cy.get('input[type="email"]').type('ruviw@mailinator.com');
+
+    cy.get('input[type="password"]').type('Pa$$w0rd!');
+
+    cy.get('.btn').click();
+
+    cy.get('nav').should('contain', 'Eaque65575');
   });
 });
