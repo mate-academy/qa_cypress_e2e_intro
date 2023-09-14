97574a1c76856eb50e6b6c4a519dd287509eb169
BohdanBondarchuk <bogdangalanet@gmail.com>
2023-09-14 18:23:37 +0300

    Final work

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,12 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io');
+    cy.get(':nth-child(2) > .nav-link').click(); 
+    cy.get(':nth-child(1) > .form-control').type('332323@i.ua');
+    cy.get(':nth-child(2) > .form-control').type('12345');
+    cy.get('.btn').click();
+    cy.get(':nth-child(4) > .nav-link').should('exist');
 
   });
 });
