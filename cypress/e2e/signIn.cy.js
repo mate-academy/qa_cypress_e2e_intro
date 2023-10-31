216f9773397bb1d50e02acb3263fb3e77ce3137d
Nataliya Hnativ <natkahnativ@gmail.com>
2023-10-31 16:31:47 +0200

    LoginTest

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,6 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-
   it('should provide an ability to log in', () => {
     cy.visit('https://react-redux.realworld.io');
     cy.get(':nth-child(2) > .nav-link')
