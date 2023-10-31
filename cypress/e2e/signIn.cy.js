a8384742eeab37e58105b8a9a95cdadd2ece5ff9
Ivan <okan_ivan@ukr.net>
2023-10-31 15:51:04 +0200

    add task solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,20 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io/');
 
+    cy.contains('a', 'Sign in').click();
+
+    cy.get(':nth-child(1) > .form-control')
+      .type('heqynypifu@mailinator.com');
+
+    cy.get(':nth-child(2) > .form-control')
+      .type('Pa$$w0rd!');
+
+    cy.get('.btn')
+      .click();
+
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', 'Nostrud quisquam eaq');
   });
 });
