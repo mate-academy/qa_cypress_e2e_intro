dae3cf7e5e137ae7ec4fcd67332b95aa60522ecc
KlymSudakov <sudakov.klym@gmail.com>
2023-10-31 22:28:29 +0200

    Fix code style

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -3,10 +3,15 @@
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
     cy.visit('https://react-redux.realworld.io');
-    cy.contains('a.nav-link', 'Sign in').click();
-    cy.get('[placeholder=Email]').type('qlym1q@mail.com');
-    cy.get('[placeholder=Password]').type('AAAAAAAA');
-    cy.get('button[type="submit"]').click();
-    cy.get('a.nav-link, [href="#qlym1q"]').should('contain', 'qlym1q');
+    cy.contains('a.nav-link', 'Sign in')
+      .click();
+    cy.get('[placeholder=Email]')
+      .type('qlym1q@mail.com');
+    cy.get('[placeholder=Password]')
+      .type('AAAAAAAA');
+    cy.get('button[type="submit"]')
+      .click();
+    cy.get('a.nav-link, [href="#qlym1q"]')
+      .should('contain', 'qlym1q');
   });
 });
