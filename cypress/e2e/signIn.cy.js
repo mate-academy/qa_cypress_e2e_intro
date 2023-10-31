53dc84747e95149337ddb98943e8e77ffd61d2b4
lognisgregnis <logsnskiy@ukr.net>
2023-10-31 17:07:21 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,16 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.visit('https://react-redux.realworld.io/#/?_k=hjk3ew');
+    cy.get(':nth-child(2) > .nav-link')
+      .click();
+    cy.get(':nth-child(1) > .form-control')
+      .type('Azazin@gmail.com');
+    cy.get(':nth-child(2) > .form-control')
+      .type('Azazin123');
+    cy.get('.btn')
+      .click();
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', 'Azazini');
   });
 });
