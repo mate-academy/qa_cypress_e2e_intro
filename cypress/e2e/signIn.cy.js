1355760324189ee86cfc1df810299c1f9d601a06
Sasha Kasianenko <kasianenkooleksa@gmail.com>
2023-10-31 17:08:28 +0200

    add solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,17 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=zczlgv');
 
+    cy.get(':nth-child(1) > .form-control')
+      .type('lurudosu@mailinator.com');
+    cy.get(':nth-child(2) > .form-control')
+      .type('Pa$$w0rd!');
+
+    cy.contains('.btn', 'Sign in')
+      .click();
+
+    cy.contains(':nth-child(4) > .nav-link', 'Consequat Dolores d')
+      .should('exist');
   });
 });
