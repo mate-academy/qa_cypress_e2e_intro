54098e27310f4cfb6e7befca55bc3b43494e343d
Serhii Oliinyk <serhiioliinykqa@gmail.com>
2023-10-31 13:03:45 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,16 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.visit('https://react-redux.realworld.io');
+    cy.get(':nth-child(2) > .nav-link')
+      .click();
+    cy.get(':nth-child(1) > .form-control')
+      .type('serhii@gmail.com');
+    cy.get(':nth-child(2) > .form-control')
+      .type('serhii');
+    cy.get('.btn')
+      .click();
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', 'serhiiolii');
   });
 });
