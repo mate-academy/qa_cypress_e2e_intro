bc413388b04f0c26f91eaef5cb43bb0b6b8941fa
Kristina Korolkova <kkorolkova94@gmail.com>
2023-10-31 14:30:06 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,14 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/login');
+  });
   it('should provide an ability to log in', () => {
-
+    cy.get(':nth-child(1) > .form-control').type('kristinatest24@gmail.com');
+    cy.get(':nth-child(2) > .form-control').type('Qwer123@');
+    cy.get('.btn').click();
+    
+    cy.get(':nth-child(4) > .nav-link').should('contain', 'kristinatest24');  
   });
 });
