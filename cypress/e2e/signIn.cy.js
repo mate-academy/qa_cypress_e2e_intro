ec990dfd53d8616d1a566968ae6d10aa3d56f0b3
Nataliya Hnativ <natkahnativ@gmail.com>
2023-10-31 16:22:45 +0200

    Test for login

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,18 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
 
+  it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io');
+    cy.get(':nth-child(2) > .nav-link')
+      .click();
+    cy.get(':nth-child(1) > .form-control')
+      .type('natusia@gmail.com');
+    cy.get(':nth-child(2) > .form-control')
+      .type('Skarb12');
+    cy.get('.btn')
+      .click();
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain', 'Natadrl');
   });
 });
