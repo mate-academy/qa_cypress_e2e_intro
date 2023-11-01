901cebdeacdebfc910f3a64282f9109e02cc8f30
qasep23-OksanaShumylko <lirokon37kop90@gmail.com>
2023-11-01 23:11:17 +0200

    Solution

--- cypress/e2e/signIn-copy-1.cy.js
+++ cypress/e2e/signIn-copy-1.cy.js
@@ -1,22 +1,22 @@
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => { 
-    cy.visit('https://react-redux.realworld.io/')
-  .get('h1')
-  .should('contain.text', 'conduit');
+/// <reference types="cypress" />
 
-  cy.get(':nth-child(2) > .nav-link')
-  .click()
-  
-  cy.get(':nth-child(1) > .form-control')
-  .type('tyui90@gmail.com');
-  
-  cy.get(':nth-child(2) > .form-control') 
-  .type( '123456789testing');
-  
-  cy.get('.btn')
-  .click();
+describe('Sign in page', ()=> {
+  it('should provide ability to log in', () =>{
+    cy.visit('https://react-redux.realworld.io/#/login?_k=oysvbs')
 
-  cy.get(':nth-child(4) > .nav-link')
-  .should('contain', 'tyuio_user');
+    cy.get(':nth-child(2) > .nav-link')
+    .click();
+
+    cy.get(':nth-child(1) > .form-control')
+    .type('tyui90@gmail.com');
+
+    cy.get(':nth-child(2) > .form-control')
+    .type('123456789testing');
+
+    cy.get('.btn')
+    .click();
+
+    cy.get(':nth-child(4) > .nav-link')
+    .should('contain.text', 'tyuio_user');
   });
 });
