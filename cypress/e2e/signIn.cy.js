02d95e05c2ab71529c71796a7bc7337ad6c273d2
Barbara Krysiak <b.krysiakk@gmail.com>
2023-11-02 12:35:47 +0100

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,25 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    const userName = 'UserUsername321';
+    const email = '3324ffdsd3@i.ua';
+    const password = 'Qwerty353421@';
 
+    before(() => {
+      cy.visit('https://react-redux.realworld.io');
+    });
+
+    it('should sign in with correct credentials', () => {
+      cy.get(':nth-child(2) > .nav-link')
+        .click();
+      cy.get(':nth-child(1) > .form-control')
+        .type(email);
+      cy.get(':nth-child(2) > .form-control')
+        .type(password);
+      cy.contains('.btn', 'Sign i')
+        .click();
+      cy.get(':nth-child(4) > .nav-link')
+        .should('contain', userName);
+    });
   });
 });
