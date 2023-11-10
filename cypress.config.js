33e79f73ef865c96d01f147115284540d5ade7d8
Mikhail Koponkin <miponkin@gmail.com>
2023-11-10 15:30:09 +0200

    solution

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://react-redux.realworld.io',
+    viewportWidth: 1024,
+    viewportHeight: 768,
     setupNodeEvents(on, config) {
     }
   }
