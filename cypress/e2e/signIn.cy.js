8821c2fad3dde7641b9e20165299669196f34b75
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2023-11-19 15:13:23 +0200

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,9 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  beforeEach(() => {
-    cy.visit('https://react-redux.realworld.io/#/login');
-  });
-
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io/#/login');
+
     cy.get('input[type="email"]').type('useralex1@gmail.com');
 
     cy.get('input[type="password"]').type('User99!$');
