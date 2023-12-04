b9e4ea6439c5c6c2f73fe8d015eba377b37047b8
Katarzyna Błaszko-Grądzik <blaszko.gradzik.k@gmail.com>
2023-12-04 15:58:59 +0100

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,24 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    const username = 'Testowy_1234';
+    const email = `${username}@mail.com`;
+    const password = 'Test1234!';
 
+    cy.visit('https://react-redux.realworld.io/#/login?_k=b320im');
+
+    cy.get('input[placeholder=Email]')
+      .type(email);
+
+    cy.get('input[placeholder=Password]')
+      .type(password);
+
+    cy.get('form button')
+      .contains('Sign in')
+      .click();
+
+    cy.get('.nav-link')
+      .contains(username)
+      .should('exist');
   });
 });
