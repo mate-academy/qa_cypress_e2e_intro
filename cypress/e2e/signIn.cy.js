b0a27d9ab3ff5d315468ca33a0235455c1a1adeb
Paweł Grzywna <pawelgrzywna01@outlook.com>
2023-12-04 15:32:13 +0100

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,13 @@
-/// <reference types="cypress" />
+/// <reference types="Cypress" />
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.visit('https://react-redux.realworld.io');
+    cy.get('div.container a').contains('Sign in').click();
+    cy.url().should('include', '#/login');
+    cy.get('[placeholder="Email"]').type('exampleuser11@mail.com');
+    cy.get('[placeholder="Password"]').type('12345');
+    cy.get('button').should('contain.text', 'Sign in').click();
+    cy.get('.navbar a').should('contain.text', 'exampleuser11');
   });
 });
