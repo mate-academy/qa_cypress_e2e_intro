6711fb57e9adedfdda84dd099e63fd5378949c25
Mateusz Gajdzinski <tulu17@gmail.com>
2023-12-04 22:59:42 +0100

    Add log in ability and assert username in header

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,24 @@
+/* eslint-disable max-len */
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  it('should provide an ability to log in and assert username in header', () => {
+    cy.visit('/');
 
+    cy.contains('Sign in')
+      .click();
+
+    cy.get('[placeholder="Email"]')
+      .type('testing@again.com');
+
+    cy.get('[placeholder="Password"]')
+      .type('testingagain17');
+
+    cy.get('button[type="submit"]')
+      .click();
+
+    cy.get('.navbar')
+      .contains('testingagain17')
+      .should('exist');
   });
 });
