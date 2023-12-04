380367c73f7271cd4c31ba8dce63ff06db932662
Wojciech Sęk <wojtasskate@wp.pl>
2023-12-04 15:26:22 +0100

    version1

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,11 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.visit('https://react-redux.realworld.io');
+    cy.get('.navbar a').contains('Sign in').click();
+    cy.get('input[type="email"]').type('kredki@kredki.com');
+    cy.get('input[type ="password"]').type('kredki');
+    cy.get('button[type="submit"]').contains('Sign in').click();
+    cy.get('.navbar').should('contain', 'kredki');
   });
 });
