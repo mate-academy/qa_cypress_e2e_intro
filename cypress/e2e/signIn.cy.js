dbdb090edbb72c750d6962c472159419a8b8a484
MagdaK <makolasze@gmail.com>
2023-12-04 16:05:18 +0100

    Cypress e2e SignIn

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,15 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.visit('https://react-redux.realworld.io');
+    cy.contains('[href="#login"]', 'Sign in')
+      .click();
+    cy.get('[placeholder="Email"]')
+      .type('matest@gmail.com');
+    cy.get('[placeholder="Password"]')
+      .type('matestmatest');
+    cy.contains('[type="submit"]', 'Sign in')
+      .click();
+    cy.contains('[href="#@matest"]', 'matest');
   });
 });
