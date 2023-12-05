c8ce918000db1c2210a0f152bc22fd21df29f6ad
Yenn-git <go.kasia78@gmail.com>
2023-12-05 14:22:14 +0100

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,23 +1,26 @@
 /// <reference types="cypress" />
 
-// const { contains } = require("cypress/types/jquery");
-
 describe('Sign In page', () => {
   beforeEach(() => {
     cy.visit('/');
   });
-  it('should provide an ability to log in', () => {
-    cy.contains('Sign in')
-      .click();
-    cy.get('[placeholder="Email"]')
-      .type('test_user_1@email.com');
-    cy.get('[placeholder="Password"]')
-      .type('123456789');
-    cy.get('[type="submit"]')
-      .click();
-    cy.url()
-      .should('contain', '/#/');
-    cy.get(':nth-child(4) > .nav-link')
-      .should('have.attr', 'href', '#@test__user_1');
-  });
+});
+
+it('should provide an ability to log in', () => {
+  const username = 'test__user_1';
+  const email = 'test_user_1@email.com';
+  const password = '123456789';
+  cy.contains('Sign in')
+    .click();
+  cy.get('[placeholder="Email"]')
+    .type(email);
+  cy.get('[placeholder="Password"]')
+    .type(password);
+  cy.get('[type="submit"]')
+    .click();
+  cy.url()
+    .should('contain', '/#/');
+  cy.get('a')
+    .contains(username)
+    .should('have.text', 'test__user_1');
 });
