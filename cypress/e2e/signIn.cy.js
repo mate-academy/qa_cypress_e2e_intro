73117294dacda8771ed5be78e6e16e05f9bd0cc3
Maciej Gajda <ma.gajda@gmail.com>
2023-12-05 17:23:43 +0100

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,16 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    const username = 'wonsz';
+    const email = 'wonsz@gmail.com';
+    const password = 'wonsz';
+    cy.visit('https://react-redux.realworld.io/#/login?_k=ww97hz');
+    cy.url().should('include', 'login');
 
+    cy.get('input[placeholder = Email]').type(email);
+    cy.get('input[placeholder = Password').type(password);
+    cy.contains('[type="submit"]', 'Sign in')
+      .click();
+    cy.get('.nav-link').contains(username).should('exist');
   });
 });
