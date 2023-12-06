6bb482517cb69602ec8e8168884d07b2c26dff69
Noemi <noemi.anna.fic@gmail.com>
2023-12-06 21:02:11 +0100

    fixed - added test case to check if the page opens

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,8 +1,16 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/login');
+  });
+
+  it('should open', () => {
+    cy.get('h1.text-xs-center')
+      .should('contain', 'Sign In');
+  });
+
   it('should provide an ability to log in', () => {
-    cy.visit('/');
     cy.get(':nth-child(1) > .form-control')
       .type('baska@baska.baska');
 
