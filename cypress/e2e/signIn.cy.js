f4b33b0bf7900a37be8766dc036f4b51f797edc8
mat12722 <mat1272295@wp.pl>
2023-12-06 12:49:56 +0100

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,23 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    const user = 'testexample321';
+    cy.visit('/');
 
+    cy.contains('Sign in')
+      .click();
+
+    cy.get('[placeholder="Email"]')
+      .type('test@example321.com');
+
+    cy.get('[placeholder="Password"]')
+      .type('passwordExaple321');
+
+    cy.get('button[type="submit"]')
+      .click();
+
+    cy.get('nav.navbar')
+      .contains(user)
+      .should('exist');
   });
 });
