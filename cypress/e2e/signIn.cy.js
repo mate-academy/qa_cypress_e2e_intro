8614f88163121649d6a24ed7f448fb4496dc9a5e
KatarzynaKolodziejczyk <kolodziejczyk.katarzyna09@gmail.com>
2023-12-11 14:41:31 +0100

    Delete  unnecessary code

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -3,23 +3,9 @@
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
     cy.visit('https://react-redux.realworld.io/');
-    cy.get('nav').should('contain.text', 'Sign up');
-    cy.get('nav').should('contain.text', 'Sign in');
-    // User Register
-    cy.get('.nav-item').get('a').contains('Sign up').click();
-    cy.url().should('contain', 'register');
-    cy.get('nav').should('contain.text', 'Sign up');
-    cy.get('body').should('exist');
-    cy.get('input[placeholder="Username"]').type('JohnSmith2010');
-    cy.get('input[placeholder="Email"]').type('JohnSmith2010@mail.com');
-    cy.get('input[placeholder="Password"]').type('JohnSmithpassword');
-    cy.get('h1.text-xs-center').click();
-    // cy.get('form button').contains('Sign up').click();
     // User Login
     cy.get('.nav-item').get('a').contains('Sign in').click();
     cy.url().should('contain', 'login');
-    cy.get('nav').should('contain.text', 'Sign in');
-    cy.get('body').should('exist');
     cy.get('input[placeholder="Email"]').type('JohnSmith2010@mail.com');
     cy.get('input[placeholder="Password"]').type('JohnSmithpassword');
     cy.get('form button').contains('Sign in').click();
