1a58d13af1ced3b9c9e562df2821ccb99194206b
Marta Kwiatkowska <marta.kwiatkowska.x@gmail.com>
2023-12-12 02:28:28 +0100

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,35 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
+  it('should provide an ability to log in', () => {
+    const username = 'cytestv2';
+    const email = 'cytestv2@gmail.com';
+    const password = 'cytestv243';
+    cy.get('.navbar a.nav-link')
+      .contains('Sign in').click();
+    cy.url()
+      .should('contain', '/#/login');
+    cy.get('body form')
+      .should('exist');
+
+    cy.intercept('GET',
+      'https://api.realworld.io/api/articles/feed?limit=10&offset=0')
+      .as('unauthorizedLogin');
+    cy.get('[placeholder="Email"]')
+      .type(email);
+    cy.get('[placeholder="Password"]')
+      .type(password);
+
+    cy.get('[type="submit"]')
+      .click();
+    cy.url()
+      .should('include', '/#/');
+    cy.get('.navbar a.nav-link')
+      .contains(username)
+      .should('have.text', 'cytestv2');
   });
 });
