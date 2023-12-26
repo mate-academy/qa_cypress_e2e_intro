1f4abb8304f3bde0109e5aeea38d0a6bac89c920
Alina Lynnyk <lnk3071129@gmail.com>
2023-12-26 16:21:58 +0200

    New Test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,14 @@
-/// <reference types="cypress" />
+// / <reference types="cypress" />
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io/#/?_k=l1s0we');
 
+    cy.get(':nth-child(2) > .nav-link').click();
+    cy.get(':nth-child(1) > .form-control').type('lnk307@gmail.com');
+    cy.get(':nth-child(2) > .form-control').type('lnk307@A');
+    cy.get('.btn').click();
+
+    cy.get(':nth-child(4) > .nav-link').contains('lnk307');
   });
 });
