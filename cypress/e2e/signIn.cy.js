9091c58c40761870c040bc1269ea5e4701342199
IvannaRA <ivankarehalova@gmail.com>
2023-12-26 15:20:15 +0300

    Зміни в signin.cy.js та cypress.config.js

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,14 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.visit('/');
+    cy.get(':nth-child(2) > .nav-link').click();
+    cy.get(':nth-child(1) > .form-control');
+    cy.get('input[type="email"]').type('ivankaregalova@gmail.com');
+    cy.get('input[type="password"]').type('111111');
+    cy.get('.btn').click();
+    cy.get(':nth-child(4) > .nav-link')
+      .contains('.nav-link', 'ivanra1')
+      .should('exist');
   });
 });
