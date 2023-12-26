4ef2b8ce68a3ce8a895c4ba75280a73b067ba9ce
SerhiiSoloviov <sergei.solovyov0294@gmail.com>
2023-12-26 13:07:11 +0200

    Testing Cypress

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,23 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+describe('Sign In Test', () => {
+  const user = {
+    email: 'a1234567890@test.ua',
+    password: 'a1234567890',
+    username: 'a1234567890',
+  };
 
+  it('should sign in and assert username in header', () => {
+    cy.visit('https://react-redux.realworld.io/#/login');
+    cy.get('input[type="email"]')
+      .type(user.email);
+    cy.get('input[type="password"]')
+      .type(user.password);
+    cy.contains('button', 'Sign in')
+      .click();
+    cy.get('.nav-item')
+      .contains('.nav-link', user.username)
+      .should('be.visible');
   });
 });
+
