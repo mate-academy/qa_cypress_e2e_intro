a5d8e2cedd62ae13ed02c394ed800b9ea2c280fe
Dmytro Horodchuk <dmytro.horodchuk@gmail.com>
2023-12-26 18:22:48 +0200

    tests added

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,16 @@
 /// <reference types="cypress" />
-
+// eslint-disable-next-line n/handle-callback-err
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false;
+});
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
-
+  it('should login with valid credentials', () => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=um8dk8');
+    cy.get(':nth-child(2) > .nav-link').click();
+    cy.get(':nth-child(1) > .form-control').type('dmytro.horodchuk@gmail.com');
+    cy.get(':nth-child(2) > .form-control').type('1234');
+    cy.get('.btn').click();
+    // eslint-disable-next-line max-len
+    cy.get(':nth-child(4) > .nav-link').should('include.text', 'dmytrohorodchuk');
   });
 });
