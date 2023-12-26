fbad6625f5c6b243f146cce8550085af90ebef88
Yuliia Siromakha <yuliya.o.siromakha@gmail.com>
2023-12-26 18:27:27 +0200

    Add cypress tests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,13 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.visit('/');
+    cy.get(':nth-child(2) > .nav-link').click();
+    cy.get(':nth-child(1) > .form-control').type ('obi@test.qa');
+    cy.get(':nth-child(2) > .form-control').type ('Qwerty!1');
+    cy.get('.btn').click();
+    cy.get('.nav-link').should('contain.text', 'obi1');
+    
   });
 });
+
