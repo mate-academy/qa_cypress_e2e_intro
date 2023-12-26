47c9aa893622058140505f44e3bcf106f47a0a3c
Nataliia Udachina <udachinanataliiava@gmail.com>
2023-12-26 17:24:46 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,26 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io');
 
+    cy.get(':nth-child(2) > .nav-link')
+      .click();
+    cy.get(':nth-child(1) > .form-control')
+      .type('yatut.coffee+1@gmail.com');
+    cy.get(':nth-child(2) > .form-control')
+      .type('P@s$w0rd');
+    cy.get('.btn')
+      .click();
+
+    cy.get(':nth-child(4) > .nav-link')
+      .contains('YaA');
+    cy.get('.container > .nav > :nth-child(2) > .nav-link')
+      .contains('New Post');
+    cy.get(':nth-child(3) > .nav-link')
+      .contains('Settings');
+
+    cy.get(':nth-child(3) > .nav-link')
+      .click();
+    cy.url().contains('settings');
   });
 });
