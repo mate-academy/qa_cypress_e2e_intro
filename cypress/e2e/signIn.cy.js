c17b6601e3044cd196a970d7cb0619fb62f95539
Orest Halushka <halslor@gmail.com>
2023-12-26 12:44:28 +0200

    new test added

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,15 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io');
 
+    cy.get(':nth-child(2) > .nav-link').click();
+
+    cy.get(':nth-child(1) > .form-control').type('sytest2@mail.com');
+    cy.get(':nth-child(2) > .form-control').type('Qwerty123456');
+
+    cy.get('.btn').click();
+
+    cy.get(':nth-child(4) > .nav-link').should('contain', 'SyTest2');
   });
 });
