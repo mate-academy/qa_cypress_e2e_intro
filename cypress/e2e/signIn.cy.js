fe4b58512ec508a328bae682445e86f1a0acf764
Alina Lynnyk <lnk3071129@gmail.com>
2023-12-26 16:30:04 +0200

    test_first

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -3,7 +3,6 @@
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
     cy.visit('https://react-redux.realworld.io/#/?_k=l1s0we');
-
     cy.get(':nth-child(2) > .nav-link').click();
     cy.get(':nth-child(1) > .form-control').type('lnk307@gmail.com');
     cy.get(':nth-child(2) > .form-control').type('lnk307@A');
