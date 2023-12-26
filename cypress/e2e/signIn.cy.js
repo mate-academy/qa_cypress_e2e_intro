091cbdcbb356bcabd0fc84c3b0029815e8f13e28
Andrii Veselovsky <andrii.veselovsky@gmail.com>
2023-12-26 15:10:34 +0200

    Testing Sign In page

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,22 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/login');
+  });
 
+  it('should provide an ability to log in', () => {
+    cy.get('h1')
+      .should('contain', 'Sign In');
+
+    cy.get('[placeholder=Email]')
+      .type('sttbd@test.ua');
+    cy.get('[placeholder=Password]')
+      .type('11');
+    cy.get('button[type=submit]')
+      .click();
+
+    cy.get('a.nav-link[href="#@testerQA"]')
+      .should('contain', 'testerQA');
   });
 });
