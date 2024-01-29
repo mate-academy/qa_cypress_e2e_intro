44895ba6a601e858da5b38902aa9411a731e959a
Barbara Krysiak <b.krysiakk@gmail.com>
2024-01-29 13:02:01 +0100

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,23 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  beforeEach(() => {
+    cy.visit('/login');
+  });
   it('should provide an ability to log in', () => {
+    cy.get('h1')
+      .should('contain.text', 'Sign In');
 
+    cy.get('[placeholder=Email]')
+      .type('bujynyg@mailinator.com');
+
+    cy.get('[placeholder=Password]')
+      .type('123456');
+
+    cy.contains('.btn', 'Sign in')
+      .click();
+
+    cy.get('.nav-link')
+      .should('contain.text', 'Polina2024');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
+/* eslint-disable eol-last */
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
