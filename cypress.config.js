107cfbb420b2f35bb242fa6a774bc3c973a25211
Anastasiia <anastassyayassnska13@gmail.com>
2024-02-02 14:09:37 +0200

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 8000,
+    baseUrl: 'http://localhost:1667',
+
     setupNodeEvents(on, config) {
     }
   }
