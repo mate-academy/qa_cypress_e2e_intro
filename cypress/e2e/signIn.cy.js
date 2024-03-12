d35997676a665cec39efbad61fccd22a1d33b1eb
falatorry <dbgdnv@gmail.com>
2024-03-12 15:28:08 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,21 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/login');
+  });
 
+  it('should provide an ability to log in', () => {
+    cy.get('[placeholder="Email"]')
+      .type('Barny45@gmail.com');
+
+    cy.get('[placeholder="Password"]')
+      .type('testpass45');
+
+    cy.get('[type="submit"]')
+      .click();
+
+    cy.get('[href="#@Barny45"]')
+      .should('contain.text', 'Barny45');
   });
 });
