1569a62ae2f1a3a5d9eda46d8e4a58be00cdd10f
Viacheslav Sova <ssovasot@gmail.com>
2024-03-12 16:33:57 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,19 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    const username = 'first_cypress_test';
+    const email = username + '@gmail.com';
+    const password = 'most_secret_password';
+    cy.visit('https://react-redux.realworld.io');
+    cy.get('[href="#login"]')
+      .click();
+    cy.get('[placeholder="Email"]')
+      .type(email);
+    cy.get('[placeholder="Password"]')
+      .type(password);
+    cy.get('[type="submit"]')
+      .click();
+    cy.get('[class="navbar navbar-light"]')
+      .should('contain.text', username);
   });
 });
