f5f8269f929f4b7b42ffdea1df968e1c3e4c9a83
Anatolii Zinevych <anatolii.zinevych@gmail.com>
2024-03-12 19:20:30 +0200

    my first e2e_ test

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    viewportWidth: 500,
+    viewportHeight: 700,
     setupNodeEvents(on, config) {
     }
   }
--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,15 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/login');
+  });
   it('should provide an ability to log in', () => {
+    const username = 'gunder111';
 
+    cy.get('[placeholder="Email"]').type('gunder111@gmail.com');
+    cy.get('[placeholder="Password"]').type('qwe1234');
+    cy.get('.btn').click();
+    cy.get('.navbar').should('contain.text', `${username}`);
   });
 });
