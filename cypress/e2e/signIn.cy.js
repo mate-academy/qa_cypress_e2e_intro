db3fff240004d973597c7d5750a1698f44585078
Oksana Kachmar <profiscribe@gmail.com>
2024-03-12 21:51:33 +0200

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,11 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
-
+  it('should provide an ability to sign in ', () => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=rju52v');
+    cy.get('input[placeholder="Email"]').type('proficypress@gmail.com');
+    cy.get('input[placeholder="Password"]').type('cypress2024!');
+    cy.get('button[type="submit"]').click();
+    cy.get('a[class="nav-link"]').should('contain.text', 'Profi');
   });
 });
