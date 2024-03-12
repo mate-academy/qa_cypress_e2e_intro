362c3ac31c0d96af194177a0cc5276356d9ee90a
Serhii Sharabura <youserhii@gmail.com>
2024-03-12 18:20:20 +0200

    test_Sign_in

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,21 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+describe('Sign In', () => {
+  const username = 'TestName';
+  before(() => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=uurats');
+  });
 
+  it('should Log in an existing user', () => {
+    cy.get('[placeholder="Email"]').first().should('exist')
+      .type(username + '@gmail.com');
+    cy.get('[placeholder="Password"]').last().should('exist').type('Aa12345!');
+    cy.get('[type="submit"]').click();
+    cy.get(':nth-child(4) > .nav-link').should('contain.text', username);
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      // returning false here prevents Cypress from
+      // failing the test
+      return false;
+    });
   });
 });
