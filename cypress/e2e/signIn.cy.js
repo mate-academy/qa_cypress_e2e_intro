3007cecba7c7e775550c7545a660758175ed7305
Tetiana Maksiuk <tetianamaksiuk@gmail.com>
2024-03-12 12:45:59 +0200

    add_test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,15 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  const EMAIL = 'gemowerez@mailinator.com';
+  const USERNAME = 'Ullamco';
+  const PASSWORD = 'qwert12345';
 
+  it('successfully login', () => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=uny8wd');
+    cy.get('[placeholder="Email"]').type(EMAIL);
+    cy.get('[placeholder="Password"]').type(PASSWORD);
+    cy.get('[type="submit"]').click();
+    cy.get('.navbar .nav-item').last().should('have.text', USERNAME);
   });
 });
