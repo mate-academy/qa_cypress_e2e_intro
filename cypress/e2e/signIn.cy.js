5a7d86ef4d13dd3227fd4045977431bd13917785
Oksana Kachmar <profiscribe@gmail.com>
2024-03-12 16:24:05 +0200

    Add test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,20 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  it('should ', () => {
+    // Відвідати сторінку "Sign in"
+    cy.visit('https://react-redux.realworld.io/#/login?_k=rju52v');
 
+    // Заповнити поле електронної пошти коректними даними
+    cy.get('input[placeholder="Email"]').type('proficypress@gmail.com');
+
+    // Заповнити поле пароля коректними даними
+    cy.get('input[placeholder="Password"]').type('cypress2024!');
+
+    // Натиснути кнопку "Sign In"
+    cy.get('button[type="submit"]').click();
+
+    // Перевірити, що ім'я користувача з'явилося у меню навігації
+    cy.get('a[class="nav-link"]').should('contain.text', 'Profi');
   });
 });
