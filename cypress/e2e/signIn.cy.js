5e4d0e05146b5adfd32cfd4939cc418fc7b801b3
Oleksii Siviolov <callebra17@gmail.com>
2024-03-16 13:02:34 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,32 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+
+    cy.on('uncaught:exception', (err, runnable) => {
+      expect(err.message).to.include('of undefined')
+        done()
+        return false
+      });
+
+    const username = 'mamkintester';
+    const email = `${username}@gmail.com`;
+    const password = '1q2w3e4R!';
+
+    cy.visit('/')
+
+    cy.get('.nav-link')
+      .contains('Sign in')
+      .click()
+
+    cy.get('[placeholder="Email"]')
+      .type(email)
+    cy.get('[placeholder="Password"]')
+      .type(password)
+    cy.get('[type="submit"]')
+      .contains('Sign in')
+      .click()
     
+    cy.contains('.nav-link', username)
+      .should('be.visible')
   });
 });
