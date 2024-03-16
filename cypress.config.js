d6773f38cf7d9849a53ea8c4b96b147abae4af3e
Oleksii Siviolov <callebra17@gmail.com>
2024-03-16 13:14:50 +0200

    Revert "Solution"
    This reverts commit 5e4d0e05146b5adfd32cfd4939cc418fc7b801b3.

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://react-redux.realworld.io',
     setupNodeEvents(on, config) {
     },
   },
