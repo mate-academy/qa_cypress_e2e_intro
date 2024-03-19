07d1749bd30ea222a18dbbd4fb3b7bee3f142cc4
ecrn erkl <ecrin.erkliii@gmail.com>
2024-03-19 11:30:43 +0200

    Homework No 1

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,39 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
 
+  it('Should provide an ability to log in', () => {
+    
+    cy.contains('a', 'Sign in')
+     .should('exist')
+     .click()
+
+     cy.url()
+      .should('include', '/login');
+
+    cy.get('h1')
+     .should('contain.text', 'Sign In')
+
+    cy.contains('a', 'Need an account?')
+     .should('exist')
+    
+    cy.get('[placeholder=Email]')
+     .should('exist')
+     .type('wiwoxuh@mailinator.com')
+
+    cy.get('[placeholder=Password]')
+     .should('exist')
+     .type('Password@1')
+
+    cy.get('button[type="submit"]')
+     .should('exist')
+     .click()
+
+    cy.get('[class="navbar navbar-light"]')
+     .last()
+     .should('contain.text', 'Libero')
   });
 });
