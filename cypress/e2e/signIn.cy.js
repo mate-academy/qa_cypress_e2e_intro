59c4d4e339c4e643aade7f68029f7cd49955163a
ecrn erkl <ecrin.erkliii@gmail.com>
2024-03-19 14:27:13 +0200

    Revert "Homework No1"
    This reverts commit 8c94749a2218fdca285ff29110ca5607cbb95798.

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -5,10 +5,9 @@ describe('Sign In page', () => {
     cy.visit('/')
   });
 
-  it('Should provide an ability to log in', () => {
+  it('should provide an ability to log in', () => {
     
     cy.contains('a', 'Sign in')
-     .should('exist')
      .click()
 
      cy.url()
@@ -16,16 +15,11 @@ describe('Sign In page', () => {
 
     cy.get('h1')
      .should('contain.text', 'Sign In')
-
-    cy.contains('a', 'Need an account?')
-     .should('exist')
     
     cy.get('[placeholder=Email]')
-     .should('exist')
      .type('wiwoxuh@mailinator.com')
 
     cy.get('[placeholder=Password]')
-     .should('exist')
      .type('Password@1')
 
     cy.get('button[type="submit"]')
