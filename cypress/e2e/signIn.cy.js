2342948f86ab74e7133281930f9235e0845c2d9a
Jakub Stein <steiniak20@gmail.com>
2024-04-19 13:42:50 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,13 @@
 /// <reference types="cypress" />
-
+beforeEach(() => {
+  cy.visit(`https://react-redux.realworld.io`);
+});
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.get('.nav-link').contains('Sign in').click();
+    cy.get('input[type="email"]').type('poczta123@wp.pl');
+    cy.get('input[type="password"]').type('Test123');
+    cy.get('button[type="submit"]').click();
+    cy.get('.nav-link').contains('steiniak');
   });
 });
