bb178166b1acf8575c75f71e04d66e03f7f3d48f
Nivon35 <Nivon35@gmail.com>
2024-04-19 12:54:37 +0200

    signIn.cy.js Implementation

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,24 @@
 /// <reference types="cypress" />
 
+before(() => {
+  cy.visit('https://react-redux.realworld.io');
+});
+
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.get('.nav-link').contains('Sign in')
+      .click();
+    cy.get('input[type="email"]')
+      .type('Testuser901@mail.com');
+    cy.get('input[type="password"]')
+      .type('Testuser901');
+    cy.get('.btn')
+      .click();
+    cy.get('.nav-link')
+      .contains('Testuser901');
   });
 });
+
+// Username: Testuser901
+// Email: Testuser901@mail.com
+// Password: Testuser901
