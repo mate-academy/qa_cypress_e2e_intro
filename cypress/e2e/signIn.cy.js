ac7ea44dca967065e606a47f9474e44bc398f7cb
domtworek <dodo7565@gmail.com>
2024-04-19 14:00:51 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,17 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  beforeEach(() => {
+    cy.visit(`https://react-redux.realworld.io/#/login`);
+  });
   it('should provide an ability to log in', () => {
+    const username = 'dtworek';
+    const email = 'dtworek@qa.team';
+    const passwd = 'dominik1234';
 
+    cy.get('[placeholder = "Email"]').type(email);
+    cy.get('[placeholder = "Password"]').type(passwd);
+    cy.get('.btn').click();
+    cy.get('.navbar-light').should('contain.text', username);
   });
 });
