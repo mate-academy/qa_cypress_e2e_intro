08491a1e493ebbb0c833699f9ed4a9eaf9556196
gajdziormax@gmail.com <gajdziormax@gmail.com>
2024-04-19 13:00:22 +0200

    e2e tests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,19 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  before(() => {
+    cy.visit('https://react-redux.realworld.io')
+  })
   it('should provide an ability to log in', () => {
+    cy.get(':nth-child(2) > .nav-link').click('topRight')
+
+    cy.get(':nth-child(1) > .form-control').type('test321321@gmail.com')
+
+    cy.get(':nth-child(2) > .form-control').type('Qwerty123')  
+
+    cy.get('.btn').click()
+
+    cy.get(':nth-child(4) > .nav-link').should('contain', 'user321321')
 
   });
 });
