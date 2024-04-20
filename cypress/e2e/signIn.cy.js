be99c22b9596e1e7bb88bd23f712c2f6f71d6753
Oleksandr Sherashov <oleksandr.sherashov@gmail.com>
2024-04-20 16:01:04 +0300

    add test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,18 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('/login');
+  });
 
+  const email = 'somemail@gmail.com';
+  const username = 'Somebody11';
+  const password = 'qwerty123';
+
+  it('should provide an ability to log in', () => {
+    cy.findByPlaceholder('Email').type(email);
+    cy.findByPlaceholder('Password').type(password);
+    cy.get('.btn').click();
+    cy.contains('a', username).should('be.visible');
   });
 });
