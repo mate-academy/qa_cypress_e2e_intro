1862b4844da2c710eef71da70b8906e0062af563
ElaOz <ozga.elzbieta94@gmail.com>
2024-04-22 23:26:53 +0200

    solution to task

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,20 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
+  it('should provide an ability to log in', () => {
+    cy.contains('.nav-link', 'Sign in')
+      .click();
+    cy.get('[placeholder="Email"]')
+      .type('xewr@mailinator.com');
+    cy.get('[placeholder="Password"]')
+      .type('9{!XxNeN$XDLkro.$');
+    cy.contains('[type="submit"]', 'Sign in')
+      .click();
+    cy.get('.nav-link')
+      .should('contain.text', 'Nihiledomn');
   });
 });
