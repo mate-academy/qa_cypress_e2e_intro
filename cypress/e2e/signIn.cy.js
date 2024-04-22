06a2dc0cdf8bb945b897ba67a34af4baca8f5819
Nivon35 <Nivon35@gmail.com>
2024-04-22 08:11:11 +0200

    signIn.cy.js - fix

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,13 +1,14 @@
 /// <reference types="cypress" />
 
-before(() => {
-  cy.visit('https://react-redux.realworld.io');
-});
-
 describe('Sign In page', () => {
+  before(() => {
+    cy.visit('https://react-redux.realworld.io');
+  });
+
   it('should provide an ability to log in', () => {
     cy.get('.nav-link').contains('Sign in')
       .click();
+    cy.url().should('include', 'login');
     cy.get('input[type="email"]')
       .type('Testuser901@mail.com');
     cy.get('input[type="password"]')
