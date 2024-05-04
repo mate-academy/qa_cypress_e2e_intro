e509951a3e843b5aa747385ced833efe1a9d4b7f
Maksym Kozhyn <mkozyn@ukr.net>
2024-05-04 16:46:30 +0300

    First task Cypress

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,23 @@
-/// <reference types="cypress" />
+/// <reference types='cypress'/>
+
+beforeEach(() => {
+  cy.visit('/');
+});
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.get(':nth-child(2) > .nav-link').contains('Sign in').click();
 
+    cy.get(':nth-child(1) > .form-control')
+      .should('have.attr', 'placeholder', 'Email')
+      .type('makskrasava@ukr.net');
+
+    cy.get(':nth-child(2) > .form-control')
+      .should('have.attr', 'placeholder', 'Password')
+      .type('K123456$k');
+
+    cy.get('.btn').click();
+
+    cy.get(':nth-child(4) > .nav-link').contains('makskrasava').should('exist');
   });
 });
