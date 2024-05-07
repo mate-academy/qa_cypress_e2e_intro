807c60e210d75565af69abd70aafc206dccaa547
Nazar Zherlitsyn <nazzherlitsyn777@gmail.com>
2024-05-07 20:29:15 +0300

    message

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,16 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=axfmw5');
+  });
   it('should provide an ability to log in', () => {
+    cy.get(':nth-child(1) > .form-control').type('455@gmail.com');
 
+    cy.get(':nth-child(2) > .form-control').type('tmn588lw');
+
+    cy.get('.btn').click();
+
+    cy.get('.nav-link').should('contain.text', 'David');
   });
 });
