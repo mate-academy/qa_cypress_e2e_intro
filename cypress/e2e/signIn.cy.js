1508a6eca5dc14378bcfac34bdaf36e0b5cf0505
Pavlo Roiev <157385125+SilverF1nger@users.noreply.github.com>
2024-05-08 12:03:17 +0300

    message

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,14 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+describe('Sign In page', () => { 
+  beforeEach (() => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=f59md1')
+  });
 
+  it('should provide an ability to log in', () => {
+   cy.get (':nth-child(1) > .form-control').type('bibobobool@gmail.com');
+   cy.get (':nth-child(2) > .form-control').type('Qwer123@');
+   cy.get ('.btn'). click();
+   cy.get (':nth-child(4) > .nav-link').should('contain.text','bibos');
   });
 });
