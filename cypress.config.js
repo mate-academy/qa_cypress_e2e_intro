988b8b4a59d073ed99a9e896f35fc01234b3bc8c
Ivan <panait.vanya@gmail.com$>
2024-05-09 15:23:37 +0300

    Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    baseUrl: 'https://react-redux.realworld.io/#' ,
+    setupNodeEvents(on, config) {  
     }
   }
 });
