1c96c9e1408ef494a2dbe1de27a1a6e12e073004
Ihor Sokolenko <igorsokolenko1986@gmail.com>
2024-05-09 13:40:59 +0300

    first_test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,10 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.visit('https://react-redux.realworld.io/#/login');
+    cy.get('input[type="email"]').type('johndoe2@gmail.com');
+    cy.get('input[type="password"]').type('Test1234');
+    cy.contains('button', 'Sign in').click();
+    cy.get(':nth-child(4) > .nav-link');
   });
 });
