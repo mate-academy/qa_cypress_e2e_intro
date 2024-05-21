e755927ed5f1b37646bd2a02b3704509711c8885
MarkelKo <davidmarkelovich@gmail.com>
2024-05-21 12:53:45 +0300

    Solt

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,17 +1,31 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
+
+describe('Demoblaze E2E Tests', () => {
+
   beforeEach(() => {
-    cy.visit('https://react-redux.realworld.io/#/login?_k=f8lexx');
+    cy.visit('https://www.demoblaze.com/');
+});
+
+  it('Should register a new user', () => {
+    cy.contains('#signin2', 'Sign up').click()
+    cy.get('#sign-username').type('MarkelKo')
+    cy.get('#sign-password').type('Qwert123@')
+    cy.get('button').contains('Sign up').click()
   });
 
-  it('should provide an ability to log in', () => {
-    cy.get(':nth-child(1) > .form-control').type('markekkk@i.ua');
-
-    cy.get(':nth-child(2) > .form-control').type('123123Qq@');
-
-    cy.get('.btn').click();
-
-    cy.get('.nav-link').should('contain.text', 'markekkk');
+  it('Should log in with an existing user', () => {
+    cy.contains('#login2', 'Log in').click()
+    cy.get('#loginusername').type('MarkelKo')
+    cy.get('#loginpassword').type('Qwert123@')
+    cy.get('button').contains('Log in').click()
   });
-})
+
+  it('Should add Samsung Galaxy s6 to the cart', () => {
+    cy.contains('Samsung galaxy s6').click()
+    cy.contains('Add to cart').click()
+    cy.on('window:alert', (txt) => {
+      expect(txt).to.contains('Product added')
+    })
+  });
+});
