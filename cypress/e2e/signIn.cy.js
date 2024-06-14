f2da4f05947bd5e859af0cdfc6d480ae37560435
Serhii Vasyliev <vv44980@gmail.com>
2024-06-14 14:01:33 -0600

    LogIn test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,24 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  it('The user should be able to log in', () => {
+    cy.visit('https://react-redux.realworld.io')
 
+    cy.contains('a', 'Sign in')
+      .click()
+
+    cy.contains('h1.text-xs-center', 'Sign In')
+
+    cy.get(':nth-child(1) > .form-control')
+      .type('user@user.ua')
+
+    cy.get(':nth-child(2) > .form-control')
+      .type('user')
+
+    cy.get('.btn')
+      .click()
+
+    cy.get('nav')
+      .contains('Test Userrr')
   });
 });
