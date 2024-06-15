a306fa4798a1de5f504ca215409cf9b5bb2c978a
Serhii Vasyliev <vv44980@gmail.com>
2024-06-15 10:38:37 -0600

    issues was fixed

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,13 +1,15 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('The user should be able to log in', () => {
+  it('should provide an ability to log in', () => {
     cy.visit('https://react-redux.realworld.io')
 
-    cy.contains('a', 'Sign in')
+    cy.get('a')
+      .contains('Sign in')
       .click()
 
-    cy.contains('h1.text-xs-center', 'Sign In')
+    cy.get('h1.text-xs-center')
+      .should('contain', 'Sign In')
 
     cy.get(':nth-child(1) > .form-control')
       .type('user@user.ua')
@@ -19,6 +21,7 @@ describe('Sign In page', () => {
       .click()
 
     cy.get('nav')
-      .contains('Test Userrr')
+      .should('contain', 'Test Userrr')
+
   });
 });
