b603f84c27b2ac89b8b1d5548ad745a8f48d454a
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-07-07 19:10:42 +0200

    tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-    }
-  }
+    baseUrl: "https://react-redux.realworld.io/#",
+    setupNodeEvents(on, config) {},
+  },
 });
