ef377fdc798b8b442b8f81c8003e099f4c41c9a8
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-07-10 07:56:27 +0300

    solution

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-    }
+    baseUrl: 'https://react-redux.realworld.io/#/'
+    // setupNodeEvents(on, config) {},
   }
 });
