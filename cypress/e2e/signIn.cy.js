bfe50ee6854bf69eccd8898c8ca6d1522e695f43
yuliayahusta <yuliayahusta@gmail.com>
2024-07-25 16:48:22 +0300

    Adding test for the Sign In form

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,12 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io/#/login');
+    cy.get(':nth-child(1) > .form-control').type('testuseryulia@gmail.com');
+    cy.get(':nth-child(2) > .form-control').type('Testuseryulia123');
 
+    cy.get('.btn').click();
+
+    cy.get(':nth-child(4) > .nav-link').should('contain.text', 'testuseryulia');
   });
 });
