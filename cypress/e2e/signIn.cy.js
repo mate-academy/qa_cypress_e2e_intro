7edd22322c0e4f863309fb25e7c65cb4aafaf047
Denys <denyssenyk2@gmail.com>
2024-07-25 14:39:30 +0300

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,10 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.visit ('https://react-redux.realworld.io/#/login?_k=67ucbn');
+    cy.get(':nth-child(1) > .form-control').type('Olena12345@gmail.com');
+    cy.get(':nth-child(2) > .form-control').type('Olena12345');
+    cy.get('.btn').click();
+    cy.get(':nth-child(4) > .nav-link').should('contain', 'olena');
   });
 });
