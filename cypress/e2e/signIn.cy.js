1216093c56aba2467b44c0b7703ddef1bc20061d
MariiaKalezhniuk <mzaharova318@gmail.com>
2024-07-25 14:00:23 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,13 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  before(() => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=jlvjml')
+  });
   it('should provide an ability to log in', () => {
-
+    cy.get(':nth-child(1) > .form-control').type('zaharova@mail.com');
+    cy.get(':nth-child(2) > .form-control').type('Zaharova1.');
+    cy.get('.btn').click();
+    cy.get('.navbar').should('contain', 'Zaharova');
   });
 });
