c3c3f2ba5a6ea41553b3d04c6e9050b8f5a74600
Kate Hubko <gubkoekaterina9592@gmail.com>
2024-08-21 21:24:13 +0300

    Cypress Inroduction

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,12 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.visit ('https://react-redux.realworld.io/#/login')
+    cy.get('[placeholder=Email]')
+      .type ( 'bekind@hja.cgj');
+    cy.get('[placeholder=Password]')
+      .type ('qweqwe');
+    cy.get('.btn').click();
+    cy.get(':nth-child(4) > .nav-link').should('contain','bekind');
   });
 });
