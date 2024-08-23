1e668d3b60e61c40bb86322b42973bf8dbeb7a54
Olena Horbatenko <gorbatenkoe73@gmail.com>
2024-08-23 13:09:09 +0100

    Fix eol-last error by adding newline at end of file

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,16 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
+  it('User can log in', () => {
+    cy.get('a').contains('Sign in').click();
+    cy.get('h1').should('contain.text', 'Sign In');
+    cy.get('[placeholder=Email]').type('dad@gmail.com');
+    cy.get('[placeholder=Password]').type('123456');
+    cy.get('.btn').click();
+    cy.get('a').contains('OlHor');
   });
 });
