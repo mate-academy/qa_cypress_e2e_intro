4be6bec3e8812786b2d0f9f6314b9b05464f13f1
Piotr <piotrwandas66@gmail.com>
2024-08-26 16:15:05 +0200

    Solution testing

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,18 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io')
+  })
+
   it('should provide an ability to log in', () => {
+    cy.get(':nth-child(2) > .nav-link').click()
+    cy.get(':nth-child(1) > .form-control').type('piotr@mail.com')
+    cy.get(':nth-child(2) > .form-control').type('haslo123456')
+
+    cy.get('.btn').click()
+
+    cy.get(':nth-child(4) > .nav-link').should('contain','piotr123456')
 
   });
 });
