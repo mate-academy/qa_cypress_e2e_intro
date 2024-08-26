1d27d7c12cdda9127c0b2695f9dd1d1b9e75e415
Daryna Riabova <darinar474@gmail.com>
2024-08-26 12:49:34 +0200

    added my first tests. cheked sign in form

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,15 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+describe("Sign In page", () => {
+  it("should provide an ability to log in", () => {
+    cy.visit("https://conduit.mate.academy/user/login");
 
+    cy.get("[type=email]").type("darinar@gmail.com");
+
+    cy.get("input[type=password]").type("Dara1234");
+
+    cy.get("[type=submit]").click();
+
+    cy.get("a.nav-link").should("contain.text", "daradara");
   });
 });
