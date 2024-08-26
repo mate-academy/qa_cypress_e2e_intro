ef48fd4dcc1575001729da634c8d57bc1c83b50b
KateAlieksieienko <kate.alieksieienko@gmail.com>
2024-08-26 19:23:41 +0200

    signIn

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,17 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
 
-  });
+describe('Sign In page', () => {
+    beforeEach(() => {
+    cy.visit('https://react-redux.realworld.io')
+    })
+
+    it('should provide an ability to log in', () => {
+      cy.get(':nth-child(2) > .nav-link').click()
+      cy.get(':nth-child(1) > .form-control').type('userK226@gmail.com')
+      cy.get(':nth-child(2) > .form-control').type('12345QW!')
+      cy.get('.btn').click()
+
+      cy.get(':nth-child(4) > .nav-link').should('contain', "userK226")
+  })
 });
