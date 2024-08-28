15e8f4616e1484b54e0b688283baec64cf2f295e
Bartłomiej Karbownik <bartlomiej.karbownik56@gmail.com>
2024-08-28 11:13:15 +0200

    Task resolved

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,15 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.visit('https://react-redux.realworld.io/#/login');
+    cy.get('input[type="email"][placeholder="Email"]')
+      .type('testname321@gmail.com');
+    cy.get('input[type="password"][placeholder="Password"]')
+      .type('test321');
+    cy.get('button.btn.btn-primary')
+      .contains('Sign in')
+      .click();
+    cy.get('a.nav-link')
+      .should('contain', 'testname321');
   });
 });
