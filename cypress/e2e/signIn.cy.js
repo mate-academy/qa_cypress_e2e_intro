88858b26c400a6005b39eec5bf266ed70b1141e4
Denys209818 <deniskravcuk63@gmail.com>
2024-09-08 00:23:36 +0300

    Load testing log in

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,22 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('/user/login');
+  });
 
+  it('should provide an ability to log in', () => {
+    cy.registerNewUser()
+      .then(({ email, password, username }) => {
+        cy.findByPlaceholder('Email')
+          .type(email);
+
+        cy.findByPlaceholder('Password')
+          .type(password + '{Enter}');
+
+        cy.visit('/');
+
+        cy.contains('a', username);
+      });
   });
 });
