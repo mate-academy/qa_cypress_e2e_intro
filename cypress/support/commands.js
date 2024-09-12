3a5b09855f4d1303f6c43cb7b96e7f14166522ac
Piotr <piotrwandas66@gmail.com>
2024-09-12 10:20:31 +0200

    Add new folder

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,26 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (email = 'piotrwandas99@gmail.com', password = 'Haslo123456!') => {
+    cy.request({
+        method: 'GET',
+        url: 'https://www.sinsay.com/pl/pl/customer/account/login/#login',
+    }).then((response) => {
+        let formKey = response.body.match(/'formKey':................'/gm).toString()
+        let formKeyArray = formKey.split(': ')
+        let formKeyValue = formKeyArray[1].toString()
+        let formKeyValueClean = formKeyValue.replace("'", "").replace("'", "")
+        cy.log(formKeyValueClean)
+        cy.request({
+            method: 'POST',
+            url: 'https://www.sinsay.com/pl/pl/ajx/customer/login/referer/aHR0cHM6Ly93d3cuc2luc2F5LmNvbS9wbC9wbC8,/uenc/aHR0cHM6Ly93d3cuc2luc2F5LmNvbS9wbC9wbC8,/?lpp_new_login',
+            form: true,
+            body: {
+                'login[username]': email,
+                'login[password]': password,
+                'form_key': formKeyValueClean
+            }
+        })
+    })
+})
