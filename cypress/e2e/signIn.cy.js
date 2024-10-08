f490af0be8d69d97ae5c713ffba351da50cf52b1
nevermore <gritsanowo@gmail.com>
2024-10-09 00:21:22 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,23 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+describe("Sign In page", () => {
+ it("should provide an ability to log in", () => {
+  const testEmail = "test_user_1@gmail.com";
+  const testPassword = "Test1234";
+  const testUserName = "test_user_1";
 
-  });
+  cy
+   .visit("https://react-redux.realworld.io")
+   .contains("a", "Sign in")
+   .should("exist")
+   .click();
+
+  cy.get("[placeholder=Email]").type(testEmail);
+
+  cy.get("[placeholder=Password]").type(testPassword);
+
+  cy.get(".btn").click();
+
+  cy.contains(testUserName).should("exist");
+ });
 });
