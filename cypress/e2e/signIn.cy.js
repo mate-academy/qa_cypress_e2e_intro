4c81e8953c659ced8960d254cc4578dde92d8dc3
Anton Zhyhanovskyi <zhyganovskiy7@gmail.com>
2024-10-08 19:55:34 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,20 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io/#/login');
+    cy.get('h1')
+      .should('contain.text', 'Sign In');
 
+    cy.get('input[placeholder="Email"]') // Email field
+      .type('anton@gmail.com');
+
+    cy.get('input[placeholder="Password"]') // Password field
+      .type('Qwerty123');
+
+    cy.get('button[type="submit"]').click(); // Sign In button
+
+    cy.get('a.nav-link')
+      .contains('Zhygan')
+      .should('be.visible');
   });
 });
