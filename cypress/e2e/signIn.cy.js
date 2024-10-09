df6a05491263c983eed46a4ce809e7bc9cf32131
Oksana Tereshchuk <oxanatereschuk@gmail.com>
2024-10-09 12:29:43 +0300

    test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,15 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://conduit.mate.academy/');
+    cy.get(':nth-child(2) > .nav-link').click();
+    cy.url().should('contain', '/login');
 
+    cy.get(':nth-child(1) > .form-control').type('oaao@gmail.com');
+    cy.get(':nth-child(2) > .form-control').type('1234qaz');
+
+    cy.get('.btn').click();
+
+    cy.get('.navbar').should('contain', 'try');
   });
 });
