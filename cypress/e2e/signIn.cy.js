6ea84d4ce9b0436c1d1c7a5f07f3155716a2bb6a
RodionPasternak <zendaliora@gmail.com>
2024-10-09 13:42:01 +0300

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,24 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://conduit.mate.academy/user/login');
 
+    cy.get('h1')
+      .should('contain.text', 'Sign in');
+
+    // email entering
+    cy.get(':nth-child(1) > .form-control')
+      .type('bybeg@mailinator.com');
+
+    // password entering
+    cy.get(':nth-child(2) > .form-control')
+      .type('Pa$$w0rd!');
+
+    cy.get('.btn')
+      .click();
+
+    cy.get(':nth-child(4) > .nav-link')
+      .contains('estetvelieaoff')
+      .should('be.visible');
   });
 });
