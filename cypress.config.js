ebcce540f8c0be7a6d6c98b62d4a9a5e9e71df60
LyudmilaDidkivska <qawsed32@ukr.net>
2024-10-09 18:18:30 +0300

    Change 2files

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,11 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-    }
-  }
+     
+    },
+    env: {
+      email: 'mate8568@ukr.net', 
+      password: 'Q1w2e3r4',      
+    },
+  },
 });
--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,14 +1,10 @@
-/// <reference types="cypress" />
-
 describe('Sign In page', () => {
-  beforeEach(()=> cy.visit('https://conduit.mate.academy/user/login'));
+  beforeEach(() => cy.visit('https://conduit.mate.academy/user/login'));
 
   it('should provide an ability to log in', () => {
-  cy.url().should('contain', 'login');
-  cy.get(':nth-child(1) > .form-control').type ('mate8568@ukr.net');
-  cy.get(':nth-child(2) > .form-control').type ('Q1w2e3r4');
-  cy.get ('.btn').click();
-  
+    cy.url().should('contain', 'login');
+    cy.get(':nth-child(1) > .form-control').type(Cypress.env('email'));
+    cy.get(':nth-child(2) > .form-control').type(Cypress.env('password'));
+    cy.get('.btn').click();
+  });
 });
-});
-
