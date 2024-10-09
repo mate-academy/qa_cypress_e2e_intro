e79bf63961049a82a9afca35ec41a0d642b2659a
mhyryak <hyryakm@gmail.com>
2024-10-09 19:51:51 +0200

    Add sign in test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,8 +2,8 @@
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
     cy.visit('https://conduit.mate.academy/user/login');
-    cy.get('input[type="email"]').type('miretta2@ukr.net');
-    cy.get('input[type="password"]').type('Miretta29+');
+    cy.get('input[type="email"]').type(Cypress.env('EMAIL'));
+    cy.get('input[type="password"]').type(Cypress.env('PASSWORD'));
     cy.get('button[type="submit"]').click();
     cy.get('.navbar').should('contain', 'miretta2');
   });
