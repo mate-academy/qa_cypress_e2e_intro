f3a9b9ba908bf8149cb60d28c0e8718efc7b07a8
Tetiana Perepelytsia <t.perepelytsya@gmail.com>
2024-10-09 16:21:25 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,14 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://conduit.mate.academy/user/login');
 
+    cy.get(':nth-child(1) > .form-control').type('test0@i.ua');
+
+    cy.get(':nth-child(2) > .form-control').type('1234567890');
+
+    cy.get('.btn').click();
+
+    cy.get(':nth-child(4) > .nav-link').should('contain', 'test0');
   });
 });
