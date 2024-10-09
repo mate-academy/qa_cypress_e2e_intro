26d70b9b50793b25d3f13f1f77c001dd03be034a
luckezQA <labchuk2007@gmail.com>
2024-10-09 13:50:48 +0300

    Solution1

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -5,13 +5,13 @@ describe('Sign In page', () => {
     cy.visit('https://conduit.mate.academy/user/login');
     cy.get('h1')
       .should('contain.text', 'Sign in');
-    cy.get(':nth-child(1) > .form-control')
+    cy.get('input[placeholder="Email"]')
       .type('luckezcy@gmail.com');
-    cy.get(':nth-child(2) > .form-control')
+    cy.get('input[placeholder="Password"]')
       .type('testPassword1!');
-    cy.get('.btn')
+    cy.get('button[type="submit"]')
       .click('bottom');
-    cy.get(':nth-child(4) > .nav-link')
+    cy.get('a.nav-link')
       .contains('luckezc');
   });
 });
