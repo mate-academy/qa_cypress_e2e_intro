e336aa9cd4fcd36aed72f0313cdd097e227593e8
Iryna Turaieva <irina.onlyjob@gmail.com>
2024-10-09 11:46:10 +0200

    Add sign-in test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,14 +1,12 @@
+// cypress/e2e/signIn.cy.js
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
     cy.visit('https://conduit.mate.academy/user/login');
-    cy.get(':nth-child(1) > .form-control')
-      .type('test111@gmail.com');
-    cy.get(':nth-child(2) > .form-control')
-      .type('Test111!');
+    cy.get(':nth-child(1) > .form-control').type('test111@gmail.com');
+    cy.get(':nth-child(2) > .form-control').type('Test111!');
     cy.get('.btn').click();
-    cy.get('.nav-link')
-      .should('contain', 'test111');
+    cy.get('.nav-link').should('contain', 'test111');
   });
 });
