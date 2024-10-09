00d84d4472b28be926ebc5f401950c2bd8b6168a
Mikola Lemeshev <mate.lemeshev@gmail.com>
2024-10-09 14:08:47 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,12 @@
-/// <reference types="cypress" />
-
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.visit('https://conduit.mate.academy/user/login');
+    cy.get('input[type="email"]').click();
+    cy.get('input[type="email"]').type('mlemeshev@i.ua');
+    cy.get('input[type="password"]').click();
+    cy.get('input[type="password"]').type('User1234');
+    cy.get('.btn').click();
+    cy.get('#__next > nav > div > ul > li:nth-child(4) > a')
+      .should('contain', 'mlemeshev');
   });
 });
