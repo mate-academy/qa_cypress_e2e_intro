8ccb4fd684fc71b1231e34326b2eaf64d0883d35
Sofi Mamzer <sofiiamamzer06@gmail.com>
2024-10-09 14:20:37 +0300

    sign in

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,10 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
-
+    cy.visit('https://conduit.mate.academy/user/login');
+    cy.get('input[type="email"]').type('sofi@gmail.com');
+    cy.get('input[type="password"]').type('Sofi06!');
+    cy.get('button[type="submit"]').click();
+    cy.get('.nav-link').should('contain', 'sofi');
   });
 });
