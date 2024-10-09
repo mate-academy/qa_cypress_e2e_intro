e1bad4788bb223a5075ecd449b5cc3ab69109525
LyudmilaDidkivska <qawsed32@ukr.net>
2024-10-09 17:59:49 +0300

    Add signIn.cy.js

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,14 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(()=> cy.visit('https://conduit.mate.academy/user/login'));
 
-  });
+  it('should provide an ability to log in', () => {
+  cy.url().should('contain', 'login');
+  cy.get(':nth-child(1) > .form-control').type ('mate8568@ukr.net');
+  cy.get(':nth-child(2) > .form-control').type ('Q1w2e3r4');
+  cy.get ('.btn').click();
+  
 });
+});
+
