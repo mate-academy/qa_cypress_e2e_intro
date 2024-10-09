0940a3ede3fa3dcb720eab0b2336b2e66b48de48
Vlad <lukashukvlad534@gmail.com>
2024-10-09 13:03:35 +0300

    Sign In page autotests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,16 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => cy.visit('https://conduit.mate.academy/user/login'));
 
+  it('should provide an ability to log in', () => {
+    cy.url().should('contain', 'user/login');
+
+    cy.get(':nth-child(1) > .form-control').type('fortest123123@gmail.com');
+    cy.get(':nth-child(2) > .form-control').type('Qwerty123.');
+
+    cy.get('.btn').click();
+
+    cy.get(':nth-child(4) > .nav-link').should('contain', 'fortest123123');
   });
 });
