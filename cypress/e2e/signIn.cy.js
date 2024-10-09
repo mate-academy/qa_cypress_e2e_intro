5db1af01854bf5d4a97eac78f7a7aa9be9435f69
mhyryak <hyryakm@gmail.com>
2024-10-09 17:19:21 +0200

    Add login test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,15 +1,18 @@
-/// <reference types="cypress" />
-
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    // Visit the Sign In page
+    cy.visit('https://react-redux.realworld.io/#/login');
 
-    cy.visit('https://conduit.mate.academy/#/login');
+    // Fill in email and password fields with data
+    cy.get('input[type=email]').type('miretta2@ukr.net');
+    cy.get('input[type=password]').type('Miretta29+');
 
-    cy.get('input[type="email"]').type('miretta2@ukr.net');
-    cy.get('input[type="password"]').type('Miretta29+');
-  
-    cy.get('button[type="submit"]').click();
-    
-    cy.get('.navbar').should('contain', 'miretta2'); 
+    // Click on [Sign In] button
+    cy.get('button[type=submit]').click();
+
+    // Assert that the username appeared in the "Site navigation (Header)" menu
+    cy.get('.nav-item', { timeout: 10000 })
+      .contains('miretta2')
+      .should('be.visible');
   });
 });
