61e4c34c53a11d820781e5b334bbbe29f36a2832
Mariana Khrustina <khrystinamariana@gmail.com>
2024-10-10 19:17:38 +0400

    Added tests for login, invalid login, and logout

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,16 +1,43 @@
-describe('Sign In Test', () => {
+describe('Login Tests', () => {
   it('Should log in with valid credentials', () => {
-    // Відвідуємо сторінку входу
-    cy.visit('https://conduit.mate.academy/user/login');
+    // Відвідуємо сторінку логіну
+    cy.visit('https://the-internet.herokuapp.com/login');
 
-    // Заповнюємо поля для email та пароля
-    cy.get('input[type="email"]').type('khrystinamariana@gmail.com');
-    cy.get('input[type="password"]').type('Sova26!');
-
-    // Натискаємо кнопку "Sign in"
+    // Вводимо валідні дані
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
     cy.get('button[type="submit"]').click();
 
-    // Перевіряємо, що ім'я користувача з'явилось в навігаційному меню (хедері)
-    cy.get('.navbar').should('contain', 'sova26');
+    // Перевірка успішного входу
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
+  });
+
+  it('Should show error with invalid credentials', () => {
+    // Відвідуємо сторінку логіну
+    cy.visit('https://the-internet.herokuapp.com/login');
+
+    // Вводимо неправильні дані
+    cy.get('#username').type('invalidUser');
+    cy.get('#password').type('invalidPassword');
+    cy.get('button[type="submit"]').click();
+
+    // Перевірка повідомлення про помилку
+    cy.get('#flash').should('contain', 'Your username is invalid!');
+  });
+
+  it('Should log out successfully', () => {
+    // Відвідуємо сторінку логіну
+    cy.visit('https://the-internet.herokuapp.com/login');
+
+    // Вводимо валідні дані
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button[type="submit"]').click();
+
+    // Натискаємо кнопку "Logout"
+    cy.get('.icon-2x.icon-signout').click();
+
+    // Перевірка успішного виходу
+    cy.get('#flash').should('contain', 'You logged out of the secure area!');
   });
 });
