47472e9f136fb1a2d2eaf75f2ecaec195a61dbf2
Oleg <_email@example.com>
2024-10-10 15:50:52 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,14 +1,5 @@
 
 describe('Sign In page', () => {
-  it('should successfully submit Sign In form', () => {
-    cy.visit('https://conduit.mate.academy/user/login');
-    cy.url().should('include', '/login');
-    cy.get('input[type="email"]').click();
-    cy.get('input[type="email"]').type('osan12345!@gmail.com');
-    cy.get('input[type="password"]').click();
-    cy.get('input[type="password"]').type('Osan12345');
-    cy.get('button[type="submit"]').click();
-  });
   it('should the username "osan" link appeared in the header "', () => {
     cy.visit('https://conduit.mate.academy/user/login');
 
