c671ba2db714dc98729b3d3c78bb0780de8865fb
vs-qaaug <strakhvleriya@gmail.com>
2024-10-11 10:04:16 +0300

    ll

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,12 @@
-/// <reference types="cypress" />
+/// <reference types="cypress" 
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
 
+    cy.visit('https://conduit.mate.academy/user/login');
+    cy.get('input[type="email"]').type('vs@gmail.com');
+    cy.get('input[type="password"]').type('Strakh05!');
+    cy.get('button[type="submit"]').click();
+    cy.get('.nav-link').should('contain', 'vs');
   });
 });
