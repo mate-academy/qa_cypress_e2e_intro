d95058c221da57dc268138d684a640103a852a7a
Rodion Pushkarskiy <pushkarskiyrodion@gmail.com>
2024-10-16 22:44:46 +0300

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,30 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  const username = 'test_name';
+  const email = 'test123456789@gmail.com';
+  const password = 'test1234';
 
+  beforeEach(() => {
+    cy.visit('/user/login');
+    cy.get('[placeholder="Email"]').type(email);
+  });
+
+  it('should log in if data is correct', () => {
+    cy.get('[placeholder="Password"]')
+      .type(password);
+    cy.get('[type="submit"]')
+      .click();
+    cy.get('[href="/profile/test_name"]')
+      .should('contain.text', username);
+  });
+
+  it('should not log in if data is not correct', () => {
+    cy.get('[placeholder="Password"]')
+      .type(password + 'incorrect');
+    cy.get('[type="submit"]')
+      .click();
+    cy.get('.error-messages > li')
+      .should('exist');
   });
 });
