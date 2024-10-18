9dcc4755fbaa577c04b82544e805a4f7dbbdae38
DmytroSaliuk <139687940+DmytroSaliuk@users.noreply.github.com>
2024-10-18 09:59:56 +0300

    Update signIn.cy.js

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,16 @@
-/// <reference types="cypress" />
+describe('signIn.cy.js', () => {
+  it('passes', () => {
+    cy.visit('https://conduit.mate.academy/')
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+    cy.get(':nth-child(2) > .nav-link').should('exist').click();
 
-  });
-});
+    cy.url().should('include', '/login')
+    cy.get('input[type="email"]').type('testsaliuk12@gmail.com')
+
+    cy.get('input[type="password"]').type('Testsaliuk12@');
+
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.nav-link').should('contain', 'testsaliuk12');
+  })
+})
