eb85c79beb1134d061a34c97072481b25ed196e8
Yuliia Dovbysh <juli3stepura@gmail.com>
2024-10-22 19:45:13 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://conduit.mate.academy',
     setupNodeEvents(on, config) {
-    }
-  }
+    },
+  },
 });
