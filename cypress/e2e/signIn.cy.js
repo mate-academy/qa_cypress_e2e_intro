9ccdad255a2d0754fb12d6e09e04ef1f14251ee4
SrTrace <levashov04sergey@gmail.com>
2024-10-24 13:40:33 +0300

    rewrite test because somehow they have gone

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,35 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
+  const user = 'zx315';
+  const email = 'zx315@mail.net';
+  const password = 'pass123';
+
+  it('should provide an ability to log in', () => {
+    cy.get('.nav-link')
+      .should('exist')
+      .should('contain.text', 'Sign in');
+
+    cy.contains('.nav-link', 'Sign in')
+      .click();
+
+    cy.url().should('include', '/login');
+    cy.get('h1').should('contain.text', 'Sign in');
+
+    cy.get('[placeholder=Email]')
+      .type(email);
+
+    cy.get('[placeholder=Password]')
+      .type(password);
+
+    cy.get('.btn-primary').click();
+
+    cy.get('.nav-link')
+      .should('exist')
+      .should('contain.text', user);
   });
 });
