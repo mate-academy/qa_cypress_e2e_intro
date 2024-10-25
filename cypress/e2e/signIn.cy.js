f3d18b13e4b7e854468046ce221e58ecc5c7b52d
Misha <banderwolfe06@gmail.com>
2024-10-25 21:07:31 +0200

    при повторному запоску потрібно змінювати логін і емаєл

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,24 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+const logUsername = 'dsarvaaav';
+const email = 'fdsfaasd13as@gmail.com';
+const password = 'dsadfgafsa';
 
+describe('my firs test', () => {
+  it('should', () => {
+    cy.visit('https://conduit.mate.academy/');
+
+    cy.get(':nth-child(3) > .nav-link').click();
+
+    cy.get(':nth-child(1) > .form-control')
+      .type(logUsername);
+
+    cy.get(':nth-child(2) > .form-control')
+      .type(email);
+
+    cy.get(':nth-child(3) > .form-control')
+      .type(password);
+
+    cy.get('.btn').click();
   });
 });
