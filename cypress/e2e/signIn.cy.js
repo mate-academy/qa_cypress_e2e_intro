35f074913d351211235726abb638782a6cbadde0
kaplunova <melamori1605@gmail.com>
2024-10-29 13:32:12 +0200

    added tests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,24 @@
-/// <reference types="cypress" />
+/// <reference types="cypress" />;
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('/user/login');
+  });
 
+  it('should provide an ability to log in', () => {
+    cy.get('h1')
+      .should('contain.text', 'Sign in');
+
+    cy.get('[placeholder=Email]')
+      .type('melamori1605@gmail.com');
+
+    cy.get('[placeholder=Password]')
+      .type('123456Test!!');
+
+    cy.get('.btn[type=submit]')
+      .click();
+
+    cy.get('.navbar-nav li:last-child .nav-link')
+      .type('calculatingmodel');
   });
 });
