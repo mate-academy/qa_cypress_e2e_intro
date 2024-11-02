bb7f77b0c18c12d3bbf1e5b978cdca5318effd6b
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-11-02 13:51:33 +0200

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,25 @@
 /// <reference types="cypress" />
 
+beforeEach('My home page', () => {
+  cy.visit('https://conduit.mate.academy/user/login');
+});
+
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.get('h1').should('contain.text', 'Sign in');
 
+    cy.get('[placeholder=Email]')
+      .type('denis_ser@gmail.com');
+
+    cy.get('[placeholder=Password]')
+      .type('1245676');
+
+    cy.get('.btn')
+      .click();
+
+    cy.contains('a', 'Global Feed')
+      .should('contain.text', 'Global Feed');
+
+    cy.url().should('equal', 'https://conduit.mate.academy');
   });
 });
