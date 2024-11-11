c64098458aee2a9a4cb6f0af8509b173c2885dd8
Roma <roma150269@gmail.com>
2024-11-11 22:53:50 +0200

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,25 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('https://conduit.mate.academy/user/login');
+  });
 
+  it('should provide an ability to log in', () => {
+    cy.url()
+      .should('include', '/login');
+  });
+
+  it('should be fill in the fields', () => {
+    cy.get(':nth-child(1) > .form-control')
+      .type('ma150269@gmail.com');
+
+    cy.get(':nth-child(2) > .form-control')
+      .type('1234');
+
+    cy.get('.btn').click();
+
+    cy.get(':nth-child(4) > .nav-link')
+      .should('contain.text', 'romaa');
   });
 });
