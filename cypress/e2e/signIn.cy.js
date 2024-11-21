a61c43544c74785b9392defb562dd92e0419ff8a
Ihor Korshylivskyi <ihorkorshylivskyi@gmail.com>
2024-11-21 12:27:31 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,18 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://conduit.mate.academy/user/login')
+      .get('h1')
+      .should('contain.text', 'Sign in');
 
+    cy.get('[type="email"]')
+      .type('ihor_test@gmail.com');
+
+    cy.get('[type="password"]')
+      .type('test1234');
+
+    cy.get('.btn').click();
+
+    cy.get('.nav-link').contains('ihortest123');
   });
 });
