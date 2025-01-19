091f67835ad217710d81e98c20dc14c43eb21749
Oksana Vasylyshyn <oksanamow@gmail.com>
2025-01-19 13:48:59 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,21 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
+  it('should provide an ability to log in', () => {
+    cy.contains('a', 'Sign in').click();
+
+    cy.url().should('include', 'login');
+
+    cy.get('[placeholder="Email"]').type('nocahol682@nongnue.com');
+
+    cy.get('[placeholder="Password"]').type('test12345');
+
+    cy.contains('button', 'Sign in').click();
+
+    cy.get('.nav-link').should('contain.text', 'dima');
   });
 });
