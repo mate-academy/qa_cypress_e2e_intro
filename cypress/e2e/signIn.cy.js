68bef326e872ac872f2eb9e1eb3e27d1bddc2115
Vladyslav Shylin <indiggoo228@gmail.com>
2025-01-24 13:32:06 +0200

    2

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,8 +1,10 @@
+import 'dotenv';
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  const email = 'indiggoo228@gmail.com';
-  const password = '1234pass';
+  const email = process.env.WEBSITE_EMAIL || 'indiggoo228@gmail.com';
+  const password = process.env.WEBSITE_PASSWORD || '1234pass';
+  const username = process.env.WEBSITE_USERNAME || 'xeomzi';
 
   it('should provide an ability to log in', () => {
     cy.visit('https://conduit.mate.academy/');
@@ -10,6 +12,6 @@ describe('Sign In page', () => {
     cy.get('[placeholder=Email]').type(email);
     cy.get('[placeholder=Password]').type(password);
     cy.contains('button', 'Sign in').click();
-    cy.contains('a', 'xeomzi');
+    cy.contains('a', username);
   });
 });
