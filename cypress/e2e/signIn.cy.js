6db5b436a18cf56b9d8efc655fd4941ca0b64052
Nataliia Henda <nataliehenda1296@gmail.com>
2025-01-30 14:23:10 +0200

    Add tests for log in

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,24 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  before(() => {
+    cy.visit('/');
+  });
 
+  it('should provide an ability to log in', () => {
+    const username = 'nataliia123';
+    const email = 'testnataliia@gmail.com';
+    const password = 'Test12345';
+    cy.contains('a', 'Sign in').should('exist').click();
+    cy.url().should('include', '/login');
+    cy.get('h1').should('contain.text', 'Sign in');
+    cy.get('[placeholder=Email]')
+      .type(email);
+    cy.get('[placeholder=Password]')
+      .type(password);
+    cy.contains('button', 'Sign in')
+      .click();
+    cy.get('.nav-link')
+      .should('contain.text', username);
   });
 });
