a4b2b1b0b5d11adfebe6aba099b95ff562796ca2
Gustavo Finch <gustavo.finch@gmail.com>
2025-02-05 13:08:39 -0300

    add task solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,1 +1,18 @@
+/// <reference types="cypress" />
+describe('Sign In page', () => {
+  it('should provide an ability to log in', () => {
+    cy.visit('https://conduit.mate.academy/user/login')
+      .get('h1')
+      .should('contain.text', 'Sign in');
 
+    cy.get('[type="email"]')
+      .type('gustavotest@gmail.com');
+
+    cy.get('[type="password"]')
+      .type('test1234');
+
+    cy.get('.btn').click();
+
+    cy.get('.nav-link').contains('ihortest123');
+  });
+});
