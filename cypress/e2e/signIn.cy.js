2ad9d3e83d81bdc1d9fc1950f5d4ff01edf6c2bc
Pawel Nowicki <pawel.nowicki87@tlen.pl>
2025-02-13 10:27:56 +0100

    add task

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,18 @@
-/// <reference types="cypress" />
+<reference types="cypress" />
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    // Odwiedzenie strony logowania
+    cy.visit('https://react-redux.realworld.io/#/login');
 
+    // Wpisanie poprawnego e-maila i hasła
+    cy.get('input[type="email"]').type('cypressTest@example.com');
+    cy.get('input[type="password"]').type('password123');
+
+    // Kliknięcie w przycisk "Sign In"
+    cy.get('button[type="submit"]').click();
+
+    // Sprawdzenie, czy nazwa użytkownika pojawiła się w nawigacji
+    cy.get('.navbar').contains('cypressTestUser').should('be.visible');
   });
 });
