17beb7f0aebda4ed4b39c8febd402ca2a2ed90af
Dmytro Ivasenko <dmtivasenko@gmail.com>
2025-02-14 15:19:57 +0400

    add solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,22 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  const username = 'testuser123';
+  const email = 'testuser123@gmail.com';
+  const password = 'testuser123password';
 
+  beforeEach(() => {
+    cy.visit('/user/login');
+  });
+
+  it('should exist', () => {
+    cy.url().should('include', 'login');
+  });
+
+  it('should log in successfully', () => {
+    cy.get('[type="email"]').type(email);
+    cy.get('[type="password"]').type(password);
+    cy.get('[type="submit"]').click();
+    cy.get(`[href="/profile/${username}"]`).should('contain.text', username);
   });
 });
