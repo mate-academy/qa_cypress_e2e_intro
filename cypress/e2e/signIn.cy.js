7e35037345d7c2ff2576293c1b89992d3797f47d
Patryk <patryk.skok@gmail.com>
2025-02-22 17:29:02 +0100

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,21 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://conduit.mate.academy/user/login')
+      .get('h1.text-xs-center')
+      .should('contain.text', 'Sign in');
 
+    cy.get('[type="email"]')
+      .type('test420@mail.com');
+
+    cy.get('[type="password"]')
+      .type('Test1234!');
+
+    cy.get('[type="submit"]')
+      .should('contain.text', 'Sign in')
+      .click();
+
+    cy.get('.navbar')
+      .should('contain.text', 'test420');
   });
 });
